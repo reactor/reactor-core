6aa2e7ca139c251869be5ded306beb0229b40b4d
Stephane Maldini <smaldini@pivotal.io>
2016-01-19 13:10:13 +0000

    fixes

--- src/main/java/reactor/core/publisher/MonoIgnoreElements.java
+++ src/main/java/reactor/core/publisher/MonoIgnoreElements.java
@@ -28,7 +28,7 @@ import reactor.core.support.ReactiveState;
  * @author Stephane Maldini
  * @since 2.5
  */
-public final class MonoIgnoreElements<IN> extends Mono<Void> implements ReactiveState.Upstream {
+public final class MonoIgnoreElements<IN> extends Mono<IN> implements ReactiveState.Upstream {
 
 	private final Publisher<IN> source;
 
@@ -42,13 +42,13 @@ public final class MonoIgnoreElements<IN> extends Mono<Void> implements Reactive
 	}
 
 	@Override
-	public void subscribe(Subscriber<? super Void> subscriber) {
+	public void subscribe(Subscriber<? super IN> subscriber) {
 		source.subscribe(new CompletableBarrier<>(subscriber));
 	}
 
-	private static class CompletableBarrier<IN> extends SubscriberBarrier<IN, Void> {
+	private static class CompletableBarrier<IN> extends SubscriberBarrier<IN, IN> {
 
-		public CompletableBarrier(Subscriber<? super Void> subscriber) {
+		public CompletableBarrier(Subscriber<? super IN> subscriber) {
 			super(subscriber);
 		}
 
