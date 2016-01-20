f8e8a4b6db18b5fd86cbfcfd08c6e5e84d9fe32a
Stephane Maldini <smaldini@pivotal.io>
2016-01-20 08:14:28 +0000

    hid FluxMap and FluxPeek

--- src/main/java/reactor/core/publisher/FluxMap.java
+++ src/main/java/reactor/core/publisher/FluxMap.java
@@ -35,7 +35,7 @@ import reactor.fn.Function;
  * {@see <a href='https://github.com/reactor/reactive-streams-commons'>https://github.com/reactor/reactive-streams-commons</a>}
  * @since 2.5
  */
-public final class FluxMap<T, R> extends Flux.FluxBarrier<T, R> {
+final class FluxMap<T, R> extends Flux.FluxBarrier<T, R> {
 
 	final Function<? super T, ? extends R> mapper;
 
