e5575fe862343460861ad65a064588b8d9ff9348
Stephane Maldini <smaldini@pivotal.io>
2016-01-26 09:04:11 +0000

    javadoc

--- src/main/java/reactor/core/publisher/package-info.java
+++ src/main/java/reactor/core/publisher/package-info.java
@@ -15,7 +15,9 @@
  */
 
 /**
- * Provide for {@link reactor.core.publisher.Flux}, {@link reactor.core.publisher.Mono} composition API and {@link org.reactivestreams.Processor} implementations
+ * Provide for
+ * {@link reactor.core.publisher.Flux}, {@link reactor.core.publisher.Mono} composition API and {@link reactor.core.publisher.Processors}
+ * implementations
  *
  * @author Stephane Maldini
  */
