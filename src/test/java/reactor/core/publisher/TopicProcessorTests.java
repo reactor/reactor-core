4c8a737e5d38f2ce20c090544c09a54667e42163
Anatoly Kadyshev <anatoly.kadyshev@gmail.com>
2016-02-03 09:01:21 +0300

    - Fix for high CPU usage after shutdown or forceShutdown

--- src/test/java/reactor/core/publisher/TopicProcessorTests.java
+++ src/test/java/reactor/core/publisher/TopicProcessorTests.java
@@ -17,6 +17,15 @@ package reactor.core.publisher;
 
 import org.junit.Test;
 import org.reactivestreams.Processor;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.test.TestSubscriber;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Stephane Maldini
@@ -69,5 +78,139 @@ public class TopicProcessorTests extends AbstractProcessorVerification {
 
 		subscriber.assertComplete();
 	}*/
+
+	@Test
+	public void testShutdownSuccessfullAfterAllDataIsRequested() throws InterruptedException {
+		TopicProcessor<String> processor = TopicProcessor.create("processor", 4);
+		Publisher<String> publisher = Flux.fromArray(new String[] { "1", "2", "3", "4", "5" });
+		publisher.subscribe(processor);
+
+		TestSubscriber<String> subscriber = new TestSubscriber<>(0);
+		processor.subscribe(subscriber);
+
+		subscriber.request(1);
+
+		Thread.sleep(250);
+
+		processor.shutdown();
+
+		assertFalse(processor.awaitAndShutdown(250, TimeUnit.MILLISECONDS));
+
+		subscriber.request(4);
+
+		assertTrue(processor.awaitAndShutdown(250, TimeUnit.MILLISECONDS));
+	}
+
+	@Test
+	public void testForceShutdownWhileWaitingForRequest() throws InterruptedException {
+		TopicProcessor<String> processor = TopicProcessor.create("processor", 4);
+		Publisher<String> publisher = Flux.fromArray(new String[] { "1", "2", "3", "4", "5" });
+		publisher.subscribe(processor);
+
+		TestSubscriber<String> subscriber = new TestSubscriber<>(0);
+		processor.subscribe(subscriber);
+
+		subscriber.request(1);
+
+		Thread.sleep(250);
+
+		processor.forceShutdown();
+
+		assertTrue(processor.awaitAndShutdown(1, TimeUnit.SECONDS));
+	}
+
+	@Test
+	public void testForceShutdownWhileWaitingForInitialRequest() throws InterruptedException {
+		TopicProcessor<String> processor = TopicProcessor.create("processor", 4);
+		Publisher<String> publisher = new CappedPublisher(2);
+		publisher.subscribe(processor);
+
+		TestSubscriber<String> subscriber = new TestSubscriber<>(0);
+		processor.subscribe(subscriber);
+
+		processor.forceShutdown();
+
+		assertTrue(processor.awaitAndShutdown(5, TimeUnit.SECONDS));
+	}
+
+
+	/**
+	 * Publishes {@link #nItems} data items in total after that any subscription.request is no-op.
+	 */
+	static class CappedPublisher implements Publisher<String> {
+
+		private Subscriber<? super String> subscriber;
+
+		private final int nItems;
+
+		public CappedPublisher(int nItems) {
+			this.nItems = nItems;
+		}
+
+		@Override
+		public void subscribe(Subscriber<? super String> s) {
+			subscriber = s;
+			s.onSubscribe(new Subscription() {
+
+				private int requested;
+
+				@Override
+				public void request(long n) {
+					long limit = Math.min(n, nItems - requested);
+
+					for (int i = 0; i < limit; i++) {
+						subscriber.onNext("" + i);
+					}
+
+					requested += limit;
+				}
+
+				@Override
+				public void cancel() {
+				}
+			});
+		}
+
+	}
+
+	@Test
+	public void testForceShutdownWhileWaitingForMoreData() throws InterruptedException {
+		TopicProcessor<String> processor = TopicProcessor.create("processor", 4);
+		Publisher<String> publisher = new CappedPublisher(2);
+		publisher.subscribe(processor);
+
+		TestSubscriber<String> subscriber = new TestSubscriber<>(0);
+		processor.subscribe(subscriber);
+
+		subscriber.request(3);
+
+		Thread.sleep(250);
+
+		processor.forceShutdown();
+
+		assertTrue(processor.awaitAndShutdown(5, TimeUnit.SECONDS));
+	}
+
+	@Test
+	public void testForceShutdownAfterShutdown() throws InterruptedException {
+		TopicProcessor<String> processor = TopicProcessor.create("processor", 4);
+		Publisher<String> publisher = Flux.fromArray(new String[] { "1", "2", "3", "4", "5" });
+		publisher.subscribe(processor);
+
+		TestSubscriber<String> subscriber = new TestSubscriber<>(0);
+		processor.subscribe(subscriber);
+
+		subscriber.request(1);
+
+		Thread.sleep(250);
+
+		processor.shutdown();
+
+		assertFalse(processor.awaitAndShutdown(250, TimeUnit.MILLISECONDS));
+
+		processor.forceShutdown();
+
+		assertTrue(processor.awaitAndShutdown(250, TimeUnit.MILLISECONDS));
+	}
 
 }
