1c5c0ec4a93e7de44042b4940b234638b9df495b
akarnokd <akarnokd@gmail.com>
2016-04-13 11:24:19 +0200

    Improve SingleConverter + tests

--- src/main/java/reactor/core/converter/RxJava1SingleConverter.java
+++ src/main/java/reactor/core/converter/RxJava1SingleConverter.java
@@ -16,10 +16,18 @@
 
 package reactor.core.converter;
 
+import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
 import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import reactor.core.flow.Fuseable;
 import reactor.core.publisher.Mono;
-import rx.Observable;
-import rx.Single;
+import reactor.core.util.*;
+import rx.*;
+import rx.internal.util.ScalarSynchronousSingle;
 
 /**
  * Convert a RxJava 1 {@link Single} to/from a Reactive Streams {@link Publisher}.
@@ -43,21 +51,253 @@ public final class RxJava1SingleConverter extends PublisherConverter<Single> {
 
 	@Override
 	public Single fromPublisher(Publisher<?> o) {
-		Observable obs = DependencyUtils.convertFromPublisher(o, Observable.class);
-		if (obs != null) {
-			return obs.toSingle();
-		}
-		return null;
+	    if (o instanceof Fuseable.ScalarSupplier) {
+            Fuseable.ScalarSupplier<?> scalarSupplier = (Fuseable.ScalarSupplier<?>) o;
+	        Object v = scalarSupplier.get();
+	        if (v == null) {
+	            return Single.error(new NoSuchElementException("Can't convert an empty Publisher to Single"));
+	        }
+	        return Single.just(v);
+	    }
+		return Single.create(new PublisherAsSingle<>(o));
 	}
 
-	@Override
-	public Mono toPublisher(Object o) {
-		Single<?> single = (Single<?>) o;
-		return Mono.from(DependencyUtils.convertToPublisher(single.toObservable()));
+	@SuppressWarnings("rawtypes")
+    @Override
+	public Mono toPublisher(Object o) { 
+	    if (o instanceof Single) {
+    		Single<?> single = (Single<?>) o;
+    		if (single instanceof ScalarSynchronousSingle) {
+    		    Object v = ((ScalarSynchronousSingle)single).get();
+    		    if (v == null) {
+    		        return Mono.error(new NullPointerException("The wrapped Single produced a null value"));
+    		    }
+                return Mono.just(v);
+    		}
+    		return new SingleAsMono<>(single);
+	    }
+	    return null;
 	}
 
 	@Override
 	public Class<Single> get() {
 		return Single.class;
+	}
+
+	/**
+	 * Wraps a Publisher and exposes it as a rx.Single, firing NoSuchElementException
+	 * for empty Publisher and IndexOutOfBoundsException if it has more than one element.
+	 *
+	 * @param <T> the value type
+	 */
+	static final class PublisherAsSingle<T> implements Single.OnSubscribe<T> {
+	    final Publisher<? extends T> source;
+	    
+        public PublisherAsSingle(Publisher<? extends T> source) {
+            this.source = source;
+        }
+
+        @Override
+	    public void call(SingleSubscriber<? super T> t) {
+            source.subscribe(new PublisherAsSingleSubscriber<>(t));
+	    }
+        
+        static final class PublisherAsSingleSubscriber<T> 
+        implements Subscriber<T>, rx.Subscription {
+            final SingleSubscriber<? super T> actual;
+            
+            Subscription s;
+            
+            boolean done;
+            
+            boolean hasValue;
+            
+            T value;
+            
+            volatile boolean terminated;
+            
+            public PublisherAsSingleSubscriber(SingleSubscriber<? super T> actual) {
+                this.actual = actual;
+            }
+            
+            @Override
+            public void onSubscribe(Subscription s) {
+                if (BackpressureUtils.validate(this.s, s)) {
+                    this.s = s;
+                    
+                    actual.add(this);
+                    
+                    s.request(Long.MAX_VALUE);
+                }
+            }
+            
+            @Override
+            public void onNext(T t) {
+                if (done) {
+                    Exceptions.onNextDropped(t);
+                    return;
+                }
+                
+                if (hasValue) {
+                    done = true;
+                    value = null;
+                    unsubscribe();
+                    
+                    actual.onError(new IndexOutOfBoundsException("The wrapped Publisher produced more than one value"));
+                    
+                    return;
+                }
+                
+                hasValue = true;
+                value = t;
+            }
+            
+            @Override
+            public void onError(Throwable t) {
+                if (done) {
+                    Exceptions.onErrorDropped(t);
+                    return;
+                }
+                done = true;
+                actual.onError(t);
+            }
+            
+            @Override
+            public void onComplete() {
+                if (done) {
+                    return;
+                }
+                done = true;
+                
+                if (hasValue) {
+                    T v = value;
+                    value = null;
+                    
+                    actual.onSuccess(v);
+                } else {
+                    actual.onError(new NoSuchElementException("The wrapped Publisher didn't produce any value"));
+                }
+            }
+            
+            @Override
+            public void unsubscribe() {
+                if (terminated) {
+                    return;
+                }
+                terminated = true;
+                s.cancel();
+            }
+            
+            @Override
+            public boolean isUnsubscribed() {
+                return terminated;
+            }
+        }
+	}
+	
+	/**
+	 * Wraps an rx.Single and exposes it as a Mono&lt;T>, signalling NullPointerException
+	 * if the Single produces a null value.
+	 *
+	 * @param <T> the value type
+	 */
+	static final class SingleAsMono<T> extends Mono<T> {
+	    final Single<? extends T> source;
+	    
+	    public SingleAsMono(Single<? extends T> source) {
+	        this.source = source;
+	    }
+	    
+	    @Override
+	    public void subscribe(Subscriber<? super T> s) {
+	        SingleAsMonoSubscriber<T> parent = new SingleAsMonoSubscriber<>(s);
+	        s.onSubscribe(parent);
+            source.subscribe(parent);
+	    }
+	    
+	    static final class SingleAsMonoSubscriber<T> extends SingleSubscriber<T> implements Subscription {
+	        final Subscriber<? super T> actual;
+	        
+	        T value;
+	        
+	        volatile int state;
+	        @SuppressWarnings("rawtypes")
+            static final AtomicIntegerFieldUpdater<SingleAsMonoSubscriber> STATE =
+	                AtomicIntegerFieldUpdater.newUpdater(SingleAsMonoSubscriber.class, "state");
+	        
+	        static final int NO_REQUEST_NO_VALUE = 0;
+            static final int NO_REQUEST_HAS_VALUE = 1;
+            static final int HAS_REQUEST_NO_VALUE = 2;
+            static final int HAS_REQUEST_HAS_VALUE = 3;
+	        
+	        public SingleAsMonoSubscriber(Subscriber<? super T> actual) {
+	            this.actual = actual;
+	        }
+	        
+	        @Override
+	        public void onSuccess(T value) {
+	            if (value == null) {
+	                actual.onError(new NullPointerException("The wrapped Single produced a null value"));
+	                return;
+	            }
+	            for (;;) {
+	                int s = state;
+	                if (s == HAS_REQUEST_HAS_VALUE || s == NO_REQUEST_HAS_VALUE || isUnsubscribed()) {
+	                    break;
+	                } else
+	                if (s == HAS_REQUEST_NO_VALUE) {
+	                    if (STATE.compareAndSet(this, s, HAS_REQUEST_HAS_VALUE)) {
+	                        actual.onNext(value);
+	                        if (!isUnsubscribed()) {
+	                            actual.onComplete();
+	                        }
+	                    }
+                        break;
+	                } else {
+	                    this.value = value;
+	                    if (STATE.compareAndSet(this, s, NO_REQUEST_HAS_VALUE)) {
+	                        break;
+	                    }
+	                }
+	            }
+	        }
+	        
+	        @Override
+	        public void onError(Throwable error) {
+	            actual.onError(error);
+	        }
+	        
+	        @Override
+	        public void request(long n) {
+	            if (BackpressureUtils.validate(n)) {
+	                for (;;) {
+	                    int s = state;
+	                    if (s == HAS_REQUEST_NO_VALUE || s == HAS_REQUEST_HAS_VALUE || isUnsubscribed()) {
+	                        break;
+	                    } else
+	                    if (s == NO_REQUEST_HAS_VALUE) {
+	                        if (STATE.compareAndSet(this, s, HAS_REQUEST_HAS_VALUE)) {
+	                            T v = value;
+	                            value = null;
+	                            actual.onNext(v);
+	                            if (!isUnsubscribed()) {
+	                                actual.onComplete();
+	                            }
+	                        }
+                            break;
+	                    } else {
+	                        if (STATE.compareAndSet(this, s, HAS_REQUEST_NO_VALUE)) {
+	                            break;
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	        
+	        @Override
+	        public void cancel() {
+	            unsubscribe();
+	        }
+	    }
 	}
 }
---
+++ src/test/java/reactor/core/converter/RxJava1SingleConverterTest.java
@@ -0,0 +1,196 @@
+package reactor.core.converter;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+import reactor.core.publisher.*;
+import reactor.core.test.TestSubscriber;
+import rx.Single;
+
+public class RxJava1SingleConverterTest {
+
+    @Test
+    public void singleToPublisherNormal() {
+        
+        Single<Integer> s = Single.just(1);
+        
+        Mono<Integer> m = RxJava1SingleConverter.from(s);
+        
+        TestSubscriber<Integer> ts = new TestSubscriber<>();
+        
+        m.subscribe(ts);
+        
+        ts.assertValues(1)
+        .assertNoError()
+        .assertComplete();
+    }
+
+    @Test
+    public void singleToPublisherNormalHidden() {
+        
+        Single<Integer> s = Single.just(1).doOnSuccess(e -> { });
+        
+        Mono<Integer> m = RxJava1SingleConverter.from(s);
+        
+        TestSubscriber<Integer> ts = new TestSubscriber<>();
+        
+        m.subscribe(ts);
+        
+        ts.assertValues(1)
+        .assertNoError()
+        .assertComplete();
+    }
+
+    @Test
+    public void singleToPublisherNormalBackpressured() {
+        
+        Single<Integer> s = Single.just(1);
+        
+        Mono<Integer> m = RxJava1SingleConverter.from(s);
+        
+        TestSubscriber<Integer> ts = new TestSubscriber<>(0);
+        
+        m.subscribe(ts);
+        
+        ts.assertNoValues()
+        .assertNoError()
+        .assertNotComplete();
+        
+        ts.request(1);
+        ts.request(1);
+        
+        ts.assertValues(1)
+        .assertNoError()
+        .assertComplete();
+    }
+    
+    @Test
+    public void singleToPublisherNull() {
+        
+        Single<Integer> s = Single.just(null);
+        
+        Mono<Integer> m = RxJava1SingleConverter.from(s);
+        
+        TestSubscriber<Integer> ts = new TestSubscriber<>();
+        
+        m.subscribe(ts);
+        
+        ts.assertNoValues()
+        .assertError(NullPointerException.class)
+        .assertNotComplete();
+    }
+
+    @Test
+    public void singleToPublisherError() {
+        
+        Single<Integer> s = Single.error(new RuntimeException("Forced failure"));
+        
+        Mono<Integer> m = RxJava1SingleConverter.from(s);
+        
+        TestSubscriber<Integer> ts = new TestSubscriber<>();
+        
+        m.subscribe(ts);
+        
+        ts.assertNoValues()
+        .assertError(RuntimeException.class)
+        .assertNotComplete();
+    }
+
+    @Test
+    public void publisherToSingleNormal() {
+        
+        Mono<Integer> m = Mono.just(1);
+        
+        Single<Integer> s = RxJava1SingleConverter.from(m);
+        
+        rx.observers.TestSubscriber<Integer> ts = new rx.observers.TestSubscriber<>();
+        
+        s.subscribe(ts);
+        
+        ts.assertValue(1);
+        ts.assertNoErrors();
+        ts.assertCompleted();
+    }
+
+    @Test
+    public void publisherToSingleNormalHidden() {
+        
+        Mono<Integer> m = Mono.just(1).doOnSuccess(e -> { });
+        
+        Single<Integer> s = RxJava1SingleConverter.from(m);
+        
+        rx.observers.TestSubscriber<Integer> ts = new rx.observers.TestSubscriber<>();
+        
+        s.subscribe(ts);
+        
+        ts.assertValue(1);
+        ts.assertNoErrors();
+        ts.assertCompleted();
+    }
+
+    @Test
+    public void publisherToSingleEmpty() {
+        
+        Mono<Integer> m = Mono.empty();
+        
+        Single<Integer> s = RxJava1SingleConverter.from(m);
+        
+        rx.observers.TestSubscriber<Integer> ts = new rx.observers.TestSubscriber<>();
+        
+        s.subscribe(ts);
+        
+        ts.assertNoValues();
+        ts.assertError(NoSuchElementException.class);
+        ts.assertNotCompleted();
+    }
+
+    @Test
+    public void publisherToSingleEmptyHidden() {
+        
+        Mono<Integer> m = Mono.<Integer>empty().doOnSuccess(e -> { });
+        
+        Single<Integer> s = RxJava1SingleConverter.from(m);
+        
+        rx.observers.TestSubscriber<Integer> ts = new rx.observers.TestSubscriber<>();
+        
+        s.subscribe(ts);
+        
+        ts.assertNoValues();
+        ts.assertError(NoSuchElementException.class);
+        ts.assertNotCompleted();
+    }
+
+    @Test
+    public void publisherToSingleMore() {
+        
+        Flux<Integer> m = Flux.just(1, 2);
+        
+        Single<Integer> s = RxJava1SingleConverter.from(m);
+        
+        rx.observers.TestSubscriber<Integer> ts = new rx.observers.TestSubscriber<>();
+        
+        s.subscribe(ts);
+        
+        ts.assertNoValues();
+        ts.assertError(IndexOutOfBoundsException.class);
+        ts.assertNotCompleted();
+    }
+
+    @Test
+    public void publisherToSingleError() {
+        
+        Mono<Integer> m = Mono.error(new RuntimeException("Forced failure"));
+        
+        Single<Integer> s = RxJava1SingleConverter.from(m);
+        
+        rx.observers.TestSubscriber<Integer> ts = new rx.observers.TestSubscriber<>();
+        
+        s.subscribe(ts);
+        
+        ts.assertNoValues();
+        ts.assertError(RuntimeException.class);
+        ts.assertNotCompleted();
+    }
+
+}
