2e20f7c1ab58bfb602a91c5cf660230e46bccefd
akarnokd <akarnokd@gmail.com>
2016-04-29 10:53:29 +0200

    DeferCompose operator proposition

---
+++ src/test/java/reactor/core/publisher/MonoDeferComposeTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2011-2016 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.core.publisher;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+
+import reactor.core.test.TestSubscriber;
+
+public class MonoDeferComposeTest {
+    @Test
+    public void perSubscriberState() {
+        
+        Mono<Integer> source = Mono.just(10).deferCompose(f -> {
+            AtomicInteger value = new AtomicInteger();
+            return f.map(v -> v + value.incrementAndGet());
+        });
+        
+        
+        for (int i = 0; i < 10; i++) {
+            TestSubscriber<Integer> ts = new TestSubscriber<>();
+            
+            source.subscribe(ts);
+            
+            ts.assertValues(11)
+            .assertComplete()
+            .assertNoError();
+        }
+    }
+    
+    @Test
+    public void composerThrows() {
+        Mono<Integer> source = Mono.just(10).deferCompose(f -> {
+            throw new RuntimeException("Forced failure");
+        });
+        
+        for (int i = 0; i < 10; i++) {
+            TestSubscriber<Integer> ts = new TestSubscriber<>();
+            
+            source.subscribe(ts);
+            
+            ts.assertNoValues()
+            .assertNotComplete()
+            .assertError(RuntimeException.class);
+        }
+        
+    }
+    
+    @Test
+    public void composerReturnsNull() {
+        Mono<Integer> source = Mono.just(10).deferCompose(f -> {
+            return null;
+        });
+        
+        for (int i = 0; i < 10; i++) {
+            TestSubscriber<Integer> ts = new TestSubscriber<>();
+            
+            source.subscribe(ts);
+            
+            ts.assertNoValues()
+            .assertNotComplete()
+            .assertError(NullPointerException.class);
+        }
+        
+    }
+}
