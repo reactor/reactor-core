1428f713d2512e26e11826199583be05c0fe3209
Simon Baslé <sbasle@pivotal.io>
2016-12-16 12:29:57 +0100

    fix #295 Add Exceptions.isOverflow
    This commit adds a private OverflowException extending
    IllegalStateException (might become public in the future).
    
    In order to test if an IllegalStateException is actually an Overflow,
    the static method isOverflow can be used.

--- src/main/java/reactor/core/Exceptions.java
+++ src/main/java/reactor/core/Exceptions.java
@@ -129,12 +129,20 @@ public abstract class Exceptions {
 	}
 
 	/**
-	 * Return an {@link IllegalStateException}
+	 * Return an {@link IllegalStateException} indicating the receiver is overrun by
+	 * more signals than expected in case of a bounded queue.
 	 *
 	 * @return an {@link IllegalStateException}
 	 */
 	public static IllegalStateException failWithOverflow() {
-		return new IllegalStateException("The receiver is overrun by more signals than " + "expected (bounded queue...)");
+		return new OverflowException("The receiver is overrun by more signals than expected (bounded queue...)");
+	}
+
+	/**
+	 * @return true if the given {@link Throwable} represents an {@link #failWithOverflow() overflow}.
+	 */
+	public static boolean isOverflow(Throwable t) {
+		return t instanceof OverflowException;
 	}
 
 	/**
@@ -344,5 +352,12 @@ public abstract class Exceptions {
 
 		private static final long serialVersionUID = 2491425227432776144L;
 
+	}
+
+	static final class OverflowException extends IllegalStateException {
+
+		OverflowException(String s) {
+			super(s);
+		}
 	}
 }
