1fed1e7a8fb77854820f607f8b5f425b2c0ab30b
sinwe <winarto@gmail.com>
2017-01-05 15:46:15 +0700

    fix #333 Mono.whenDelayError with 3 params only uses last 2

--- src/main/java/reactor/core/publisher/Mono.java
+++ src/main/java/reactor/core/publisher/Mono.java
@@ -861,7 +861,7 @@ public abstract class Mono<T> implements Publisher<T> {
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static <T1, T2, T3> Mono<Tuple3<T1, T2, T3>> whenDelayError(Mono<? extends T1> p1, Mono<? extends T2> p2, Mono<? extends T3> p3) {
-		return onAssembly(new MonoWhen(true, a -> Tuples.fromArray((Object[])a), p2, p3));
+		return onAssembly(new MonoWhen(true, a -> Tuples.fromArray((Object[])a), p1, p2, p3));
 	}
 
 	/**
