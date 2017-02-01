dae914c3e3978095cf05c326246fe46a28374179
Simon Baslé <sbasle@pivotal.io>
2017-02-01 11:48:28 +0100

    most refguide snippets are now unit tested

--- src/test/java/reactor/core/publisher/scenarios/GuideTests.java
+++ src/test/java/reactor/core/publisher/scenarios/GuideTests.java
@@ -16,16 +16,41 @@
 
 package reactor.core.publisher.scenarios;
 
+import java.io.IOException;
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.Disposable;
+import reactor.core.Exceptions;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Operators;
+import reactor.core.publisher.SignalType;
 import reactor.core.publisher.UnicastProcessor;
+import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
+ * Tests mirroring snippets from the reference documentation.
+ *
  * @author Stephane Maldini
+ * @author Simon Baslé
  */
 public class GuideTests {
 
@@ -107,7 +132,484 @@ public class GuideTests {
 		hotSource.onNext("orange");
 		hotSource.onNext("purple");
 		hotSource.onComplete();
+	}
 
+	private Flux<String> someStringSource() {
+		return Flux.just("foo", "bar", "baz").hide();
+	}
+
+	@Test
+	public void baseSubscriberFineTuneBackpressure() {
+		Flux<String> source = someStringSource();
+
+		source.map(String::toUpperCase)
+		      .subscribe(new BaseSubscriber<String>() { // <1>
+			      @Override
+			      protected void hookOnSubscribe(Subscription subscription) {
+				      // <2>
+				      request(1); // <3>
+			      }
+
+			      @Override
+			      protected void hookOnNext(String value) {
+				      request(1); // <4>
+			      }
+
+			      //<5>
+		      });
+	}
+
+	private String doSomethingDangerous(long i) {
+		if (i < 5)
+			return String.valueOf(i);
+		throw new IllegalArgumentException("boom" + i);
+	}
+
+	private String doSecondTransform(String i) {
+		return "item" + i;
+	}
+
+	@Test
+	public void errorHandlingOnError() {
+		Flux<String> s = Flux.range(1, 10)
+		                     .map(v -> doSomethingDangerous(v)) // <1>
+		                     .map(v -> doSecondTransform(v)); // <2>
+		s.subscribe(value -> System.out.println("RECEIVED " + value), // <3>
+				error -> System.err.println("CAUGHT " + error) // <4>
+		);
+
+		StepVerifier.create(s)
+	                .expectNext("item1")
+	                .expectNext("item2")
+	                .expectNext("item3")
+	                .expectNext("item4")
+	                .verifyErrorMessage("boom5");
+	}
+
+	@Test
+	public void errorHandlingTryCatch() {
+		try {
+			for (int i = 1; i < 11; i++) {
+				String v1 = doSomethingDangerous(i); // <1>
+				String v2 = doSecondTransform(v1); // <2>
+				System.out.println("RECEIVED " + v2);
+			}
+		} catch (Throwable t) {
+			System.err.println("CAUGHT " + t); // <3>
+		}
+	}
+
+	@Test
+	public void errorHandlingReturn() {
+		Flux<String> flux =
+		Flux.just(10)
+		    .map(this::doSomethingDangerous)
+		    .onErrorReturn("RECOVERED");
+
+		StepVerifier.create(flux)
+	                .expectNext("RECOVERED")
+	                .verifyComplete();
+	}
+
+	@Test
+	public void errorHandlingReturnFilter() {
+		Flux<String> flux =
+		Flux.just(10)
+		    .map(this::doSomethingDangerous)
+		    .onErrorReturn(e -> e.getMessage().equals("boom10"), "recovered10");
+
+		StepVerifier.create(flux)
+	                .expectNext("recovered10")
+	                .verifyComplete();
+
+		flux =
+		Flux.just(9)
+		    .map(this::doSomethingDangerous)
+		    .onErrorReturn(e -> e.getMessage().equals("boom10"), "recovered10");
+
+		StepVerifier.create(flux)
+	                .verifyErrorMessage("boom9");
+	}
+
+	private Flux<String> callExternalService(String key) {
+		if (key.equals("key2"))
+			return Flux.error(new IllegalStateException("boom"));
+		if (key.startsWith("timeout"))
+			return Flux.error(new TimeoutException());
+		if (key.startsWith("unknown"))
+			return Flux.error(new UnknownKeyException());
+		return Flux.just(key.replace("key", "value"));
+	}
+
+	private Flux<String> getFromCache(String key) {
+		return Flux.just("outdated" + key);
+	}
+
+	@Test
+	public void errorHandlingSwitchOnError() {
+		Flux<String> flux =
+				Flux.just("key1", "key2")
+				    .flatMap(k ->
+						    callExternalService(k) // <1>
+								    .switchOnError(getFromCache(k)) // <2>
+				    );
+
+		StepVerifier.create(flux)
+	                .expectNext("value1", "outdatedkey2")
+	                .verifyComplete();
+	}
+
+	private class UnknownKeyException extends RuntimeException {
+
+	}
+
+	private Flux<String> registerNewEntry(String key, String value) {
+		return Flux.just(key + "=" + value);
+	}
+
+	@Test
+	public void errorHandlingResumeWith() {
+		Flux<String> flux =
+		Flux.just("timeout1", "unknown", "key2")
+		    .flatMap(k ->
+				    callExternalService(k)
+						    .onErrorResumeWith(error -> { // <1>
+							    if (error instanceof TimeoutException) // <2>
+								    return getFromCache(k);
+							    else if (error instanceof UnknownKeyException) // <3>
+								    return registerNewEntry(k, "DEFAULT");
+							    else
+								    return Flux.error(error); // <4>
+						    })
+		    );
+
+		StepVerifier.create(flux)
+	                .expectNext("outdatedtimeout1")
+	                .expectNext("unknown=DEFAULT")
+	                .verifyErrorMessage("boom");
+	}
+
+	private class BusinessException extends RuntimeException {
+
+		public BusinessException(String message, Throwable cause) {
+			super(message, cause);
+		}
+	}
+
+	@Test
+	public void errorHandlingRethrow() {
+		Flux<String> flux =
+		Flux.just("timeout1")
+		    .flatMap(k -> callExternalService(k)
+				    .onErrorResumeWith(original -> Flux.error(
+						    new BusinessException("oops, SLA exceeded", original))
+				    )
+		    );
+
+		StepVerifier.create(flux)
+		            .verifyErrorMatches(e -> e instanceof BusinessException &&
+				            e.getMessage().equals("oops, SLA exceeded") &&
+				            e.getCause() instanceof TimeoutException);
+	}
+
+	private void log(String s) {
+		System.out.println(s);
+	}
+
+	@Test
+	public void errorHandlingSideEffect() {
+		LongAdder failureStat = new LongAdder();
+		Flux<String> flux =
+		Flux.just("unknown")
+		    .flatMap(k -> callExternalService(k) // <1>
+				    .doOnError(e -> {
+				        failureStat.increment();
+				        log("uh oh, falling back, service failed for key " + k); // <2>
+				    })
+		        .switchOnError(getFromCache(k)) // <3>
+		    );
+
+		StepVerifier.create(flux)
+	                .expectNext("outdatedunknown")
+	                .verifyComplete();
+
+		assertThat(failureStat.intValue()).isEqualTo(1);
+	}
+
+	@Test
+	public void errorHandlingUsing() {
+		AtomicBoolean isDisposed = new AtomicBoolean();
+		Disposable disposableInstance = new Disposable() {
+			@Override
+			public void dispose() {
+				isDisposed.set(true); // <4>
+			}
+
+			@Override
+			public String toString() {
+				return "DISPOSABLE";
+			}
+		};
+
+		Flux<String> flux =
+		Flux.using(
+				() -> disposableInstance, // <1>
+				disposable -> Flux.just(disposable.toString()), // <2>
+				Disposable::dispose // <3>
+		);
+
+		StepVerifier.create(flux)
+	                .expectNext("DISPOSABLE")
+	                .verifyComplete();
+
+		assertThat(isDisposed.get()).isTrue();
+	}
+
+	@Test
+	public void errorHandlingDoFinally() {
+		LongAdder statsCancel = new LongAdder(); // <1>
+
+		Flux<String> flux =
+				Flux.just("foo", "bar")
+				    .doFinally(type -> {
+					    if (type == SignalType.CANCEL) // <2>
+						    statsCancel.increment(); // <3>
+				    })
+				    .take(1); // <4>
+
+		StepVerifier.create(flux)
+	                .expectNext("foo")
+	                .verifyComplete();
+
+		assertThat(statsCancel.intValue()).isEqualTo(1);
+	}
+
+	@Test
+	public void errorHandlingIntervalMillisNotContinued() throws InterruptedException {
+		VirtualTimeScheduler virtualTimeScheduler = VirtualTimeScheduler.createForAll();
+		VirtualTimeScheduler.set(virtualTimeScheduler);
+
+		Flux<String> flux =
+		Flux.intervalMillis(250)
+		    .map(input -> {
+			    if (input < 3) return "tick " + input;
+			    throw new RuntimeException("boom");
+		    })
+		    .onErrorReturn("Uh oh");
+
+		flux.subscribe(System.out::println);
+		//Thread.sleep(2100); // <1>
+
+		virtualTimeScheduler.advanceTimeBy(Duration.ofHours(1));
+
+		StepVerifier.withVirtualTime(() -> flux, () -> virtualTimeScheduler, Long.MAX_VALUE)
+	                .thenAwait(Duration.ofSeconds(3))
+	                .expectNext("tick 0")
+	                .expectNext("tick 1")
+	                .expectNext("tick 2")
+	                .expectNext("Uh oh")
+	                .verifyComplete();
+	}
+
+	@Test
+	public void errorHandlingIntervalMillisRetried() throws InterruptedException {
+		VirtualTimeScheduler virtualTimeScheduler = VirtualTimeScheduler.createForAll();
+		VirtualTimeScheduler.set(virtualTimeScheduler);
+
+		Flux<Tuple2<Long,String>> flux =
+		Flux.intervalMillis(250)
+		    .map(input -> {
+			    if (input < 3) return "tick " + input;
+			    throw new RuntimeException("boom");
+		    })
+		    .elapsed() // <1>
+		    .retry(1);
+
+		flux.subscribe(System.out::println,
+				System.err::println); // <2>
+
+		//Thread.sleep(2100); // <3>
+
+		virtualTimeScheduler.advanceTimeBy(Duration.ofHours(1));
+
+		StepVerifier.withVirtualTime(() -> flux, () -> virtualTimeScheduler, Long.MAX_VALUE)
+		            .thenAwait(Duration.ofSeconds(3))
+		            .expectNextMatches(t -> t.getT2().equals("tick 0"))
+		            .expectNextMatches(t -> t.getT2().equals("tick 1"))
+		            .expectNextMatches(t -> t.getT2().equals("tick 2"))
+		            .expectNextMatches(t -> t.getT2().equals("tick 0"))
+		            .expectNextMatches(t -> t.getT2().equals("tick 1"))
+		            .expectNextMatches(t -> t.getT2().equals("tick 2"))
+		            .verifyErrorMessage("boom");
+	}
+
+	public String convert(int i) throws IOException {
+		if (i > 3) {
+			throw new IOException("boom " + i);
+		}
+		return "OK " + i;
+	}
+
+	@Test
+	public void errorHandlingPropagateUnwrap() {
+		Flux<String> converted = Flux
+				.range(1, 10)
+				.map(i -> {
+					try { return convert(i); }
+					catch (IOException e) { throw Exceptions.propagate(e); }
+				});
+
+		converted.subscribe(
+				v -> System.out.println("RECEIVED: " + v),
+				e -> {
+					if (Exceptions.unwrap(e) instanceof IOException) {
+						System.out.println("Something bad happened with I/O");
+					} else {
+						System.out.println("Something bad happened");
+					}
+				}
+		);
+
+		StepVerifier.create(converted)
+	                .expectNext("OK 1")
+	                .expectNext("OK 2")
+	                .expectNext("OK 3")
+	                .verifyErrorMessage("boom 4");
+	}
+
+	@Test
+	public void producingGenerate() {
+		Flux<String> flux = Flux.generate(
+				() -> 0, // <1>
+				(state, sink) -> {
+					sink.next("3 x " + state + " = " + 3*state); // <2>
+					if (state == 10) sink.complete(); // <3>
+					return state + 1; // <4>
+				});
+
+		StepVerifier.create(flux)
+	                .expectNext("3 x 0 = 0")
+	                .expectNext("3 x 1 = 3")
+	                .expectNext("3 x 2 = 6")
+	                .expectNext("3 x 3 = 9")
+	                .expectNext("3 x 4 = 12")
+	                .expectNext("3 x 5 = 15")
+	                .expectNext("3 x 6 = 18")
+	                .expectNext("3 x 7 = 21")
+	                .expectNext("3 x 8 = 24")
+	                .expectNext("3 x 9 = 27")
+	                .expectNext("3 x 10 = 30")
+	                .verifyComplete();
+	}
+
+
+	@Test
+	public void producingGenerateMutableState() {
+		Flux<String> flux = Flux.generate(
+				AtomicLong::new, // <1>
+				(state, sink) -> {
+					long i = state.getAndIncrement(); // <2>
+					sink.next("3 x " + i + " = " + 3*i);
+					if (i == 10) sink.complete();
+					return state; // <3>
+				});
+
+		StepVerifier.create(flux)
+	                .expectNext("3 x 0 = 0")
+	                .expectNext("3 x 1 = 3")
+	                .expectNext("3 x 2 = 6")
+	                .expectNext("3 x 3 = 9")
+	                .expectNext("3 x 4 = 12")
+	                .expectNext("3 x 5 = 15")
+	                .expectNext("3 x 6 = 18")
+	                .expectNext("3 x 7 = 21")
+	                .expectNext("3 x 8 = 24")
+	                .expectNext("3 x 9 = 27")
+	                .expectNext("3 x 10 = 30")
+	                .verifyComplete();
+	}
+
+	interface MyEventListener<T> {
+		void onDataChunk(List<T> chunk);
+		void processComplete();
+	}
+
+	interface MyEventProcessor {
+		void register(MyEventListener<String> eventListener);
+		void dataChunk(String... values);
+		void processComplete();
+	}
+
+	private MyEventProcessor myEventProcessor = new MyEventProcessor() {
+
+		private MyEventListener<String> eventListener;
+
+		@Override
+		public void register(MyEventListener<String> eventListener) {
+			this.eventListener = eventListener;
+		}
+
+		@Override
+		public void dataChunk(String... values) {
+			eventListener.onDataChunk(Arrays.asList(values));
+		}
+
+		@Override
+		public void processComplete() {
+			eventListener.processComplete();
+		}
+	};
+
+	@Test
+	public void producingCreate() {
+		Flux<String> bridge = Flux.create(sink -> {
+			myEventProcessor.register( // <4>
+					new MyEventListener<String>() { // <1>
+
+						public void onDataChunk(List<String> chunk) {
+							for(String s : chunk) {
+								sink.next(s); // <2>
+							}
+						}
+
+						public void processComplete() {
+							sink.complete(); // <3>
+						}
+					});
+		});
+
+		StepVerifier.withVirtualTime(() -> bridge)
+	                .expectSubscription()
+	                .expectNoEvent(Duration.ofSeconds(10))
+	                .then(() -> myEventProcessor.dataChunk("foo", "bar", "baz"))
+	                .expectNext("foo", "bar", "baz")
+	                .expectNoEvent(Duration.ofSeconds(10))
+	                .then(() -> myEventProcessor.processComplete())
+	                .verifyComplete();
+	}
+
+	public String alphabet(int letterNumber) {
+		if (letterNumber < 1 || letterNumber > 26) {
+			return null;
+		}
+		int letterIndexAscii = 'A' + letterNumber - 1;
+		return "" + (char) letterIndexAscii;
+	}
+
+	@Test
+	public void producingHandle() {
+		Flux<String> alphabet = Flux.just(-1, 30, 13, 9, 20)
+            .handle((i, sink) -> {
+                String letter = alphabet(i); // <1>
+                if (letter != null) // <2>
+                        sink.next(letter); // <3>
+            });
+
+		alphabet.subscribe(System.out::println);
+
+		StepVerifier.create(alphabet)
+	                .expectNext("M", "I", "T")
+	                .verifyComplete();
 	}
 
 }
