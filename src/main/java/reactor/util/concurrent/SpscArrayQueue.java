ee1170f5939396e55579e862cb8147ca8bd1bf53
Stephane Maldini <smaldini@pivotal.io>
2017-02-05 21:37:20 +0000

    Fix linked queue minimum (was always min !) and bi-insert

--- src/main/java/reactor/util/concurrent/SpscArrayQueue.java
+++ src/main/java/reactor/util/concurrent/SpscArrayQueue.java
@@ -37,7 +37,7 @@ final class SpscArrayQueue<T> extends SpscArrayQueueP3<T> implements Queue<T> {
 	/** */
 	private static final long serialVersionUID = 494623116936946976L;
 
-	public SpscArrayQueue(int capacity) {
+	SpscArrayQueue(int capacity) {
 		super(QueueSupplier.ceilingNextPowerOfTwo(capacity));
 	}
 	
