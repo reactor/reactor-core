9b69137ca64a1284751d81fc03b2743b82b7ea48
Stephane Maldini <smaldini@pivotal.io>
2017-03-10 13:41:51 +0000

    Rick and Morty have decided, Simon Basle must be spelt without accent

--- reactor-test/src/main/java/reactor/test/publisher/TestPublisher.java
+++ reactor-test/src/main/java/reactor/test/publisher/TestPublisher.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package reactor.test.publisher;
 
 import java.util.Objects;
@@ -20,7 +35,7 @@ import reactor.core.publisher.Mono;
  * {@link #createNoncompliant(Violation, Violation...)} factory method to create such
  * a misbehaving publisher.
  *
- * @author Simon Baslé
+ * @author Simon Basle
  */
 public abstract class TestPublisher<T> implements Publisher<T> {
 
--- reactor-test/src/test/java/reactor/test/StepVerifierTests.java
+++ reactor-test/src/test/java/reactor/test/StepVerifierTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2016 Pivotal Software Inc, All Rights Reserved.
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ import static reactor.test.publisher.TestPublisher.Violation.REQUEST_OVERFLOW;
  * @author Arjen Poutsma
  * @author Sebastien Deleuze
  * @author Stephane Maldini
- * @author Simon Baslé
+ * @author Simon Basle
  */
 public class StepVerifierTests {
 
