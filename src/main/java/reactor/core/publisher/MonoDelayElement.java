dc4a07ecbaa52434bd98b7b5807bf6645249b8b3
Stephane Maldini <smaldini@pivotal.io>
2017-03-24 15:26:21 +0000

    feedback #3

--- src/main/java/reactor/core/publisher/MonoDelayElement.java
+++ src/main/java/reactor/core/publisher/MonoDelayElement.java
@@ -31,7 +31,7 @@ import reactor.core.scheduler.Scheduler;
  *
  * @see <a href="https://github.com/reactor/reactive-streams-commons">Reactive-Streams-Commons</a>
  * @author Simon Baslé
- * NOTE : Review impl
+ * TODO : Review impl
  */
 final class MonoDelayElement<T> extends MonoSource<T, T> {
 
