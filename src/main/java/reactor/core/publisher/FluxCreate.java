da8517e240614ce33dd2496af96284df643ad470
Stephane Maldini <smaldini@pivotal.io>
2017-04-13 11:00:31 +0100

    Add optional OverflowStrategy to FluxProcessor.sink

--- src/main/java/reactor/core/publisher/FluxCreate.java
+++ src/main/java/reactor/core/publisher/FluxCreate.java
@@ -61,32 +61,29 @@ final class FluxCreate<T> extends Flux<T> {
 		this.createMode = createMode;
 	}
 
-	@Override
-	public void subscribe(Subscriber<? super T> t) {
-		BaseSink<T> sink;
-
+	static <T> BaseSink<T> createSink(Subscriber<? super T> t, OverflowStrategy
+			backpressure){
 		switch (backpressure) {
 			case IGNORE: {
-				sink = new IgnoreSink<>(t);
-				break;
+				return new IgnoreSink<>(t);
 			}
 			case ERROR: {
-				sink = new ErrorAsyncSink<>(t);
-				break;
+				return new ErrorAsyncSink<>(t);
 			}
 			case DROP: {
-				sink = new DropAsyncSink<>(t);
-				break;
+				return new DropAsyncSink<>(t);
 			}
 			case LATEST: {
-				sink = new LatestAsyncSink<>(t);
-				break;
+				return new LatestAsyncSink<>(t);
 			}
 			default: {
-				sink = new BufferAsyncSink<>(t, QueueSupplier.SMALL_BUFFER_SIZE);
-				break;
+				return new BufferAsyncSink<>(t, QueueSupplier.SMALL_BUFFER_SIZE);
 			}
 		}
+	}
+	@Override
+	public void subscribe(Subscriber<? super T> t) {
+		BaseSink<T> sink = createSink(t, backpressure);
 
 		t.onSubscribe(sink);
 		try {
