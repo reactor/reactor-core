45ecd8431e021465b10e01fe5d61235b2bc2d056
Stephane Maldini <smaldini@pivotal.io>
2017-04-26 16:59:33 +0100

    fix #564 Add Parallel#subscribe alias /!\ Fix final API on subscribe(Consumer)

--- src/test/java/reactor/core/publisher/ParallelFluxTest.java
+++ src/test/java/reactor/core/publisher/ParallelFluxTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2016 Pivotal Software Inc, All Rights Reserved.
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -334,9 +334,7 @@ public class ParallelFluxTest {
 		                                 .parallel(3)
 		                                 .doOnEach(s -> s.accept(peekSubscriber));
 
-		//we use a lambda subscriber and latch to avoid using `sequential`
-		flux.subscribe(v -> {
-		});
+		flux.subscribe();
 
 		peekSubscriber.assertNotSubscribed();
 		peekSubscriber.assertValues(1, 2);
