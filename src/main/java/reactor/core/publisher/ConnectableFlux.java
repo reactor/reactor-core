f98eb130722151003b35e8a279839156db44a812
Stephane Maldini <smaldini@pivotal.io>
2017-05-26 12:19:31 -0700

    fix #624 open Scheduler argument for refCount

--- src/main/java/reactor/core/publisher/ConnectableFlux.java
+++ src/main/java/reactor/core/publisher/ConnectableFlux.java
@@ -20,6 +20,7 @@ import java.util.function.Consumer;
 
 import reactor.core.Disposable;
 import reactor.core.Fuseable;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 /**
@@ -157,7 +158,28 @@ public abstract class ConnectableFlux<T> extends Flux<T> {
 	 * @return a reference counting {@link Flux} with a grace period for disconnection
 	 */
 	public final Flux<T> refCount(int minSubscribers, Duration gracePeriod) {
-		return onAssembly(new FluxRefCountGrace<>(this, minSubscribers, gracePeriod, Schedulers.elastic()));
+		return refCount(minSubscribers, gracePeriod, Schedulers.elastic());
+	}
+
+	/**
+	 * Connects to the upstream source when the given number of {@link org.reactivestreams.Subscriber} subscribes.
+	 * Disconnection can happen in two scenarios: when the upstream source completes (or errors) then
+	 * there is an immediate disconnection. However, when all subscribers have cancelled,
+	 * a <strong>deferred</strong> disconnection is scheduled. If any new subscriber comes
+	 * in during the {@code gracePeriod} that follows, the disconnection is cancelled.
+	 *
+	 * <p>
+	 * <img class="marble" src="https://raw.githubusercontent.com/reactor/reactor-core/v3.1.0.M1/src/docs/marble/refCount.png" alt="">
+	 *
+	 * @param minSubscribers the number of subscribers expected to subscribe before connection
+	 * @param gracePeriod the {@link Duration} for which to wait for new subscribers before actually
+	 * disconnecting when all subscribers have cancelled.
+	 * @param scheduler the {@link Scheduler} on which to run timeouts
+	 *
+	 * @return a reference counting {@link Flux} with a grace period for disconnection
+	 */
+	public final Flux<T> refCount(int minSubscribers, Duration gracePeriod, Scheduler scheduler) {
+		return onAssembly(new FluxRefCountGrace<>(this, minSubscribers, gracePeriod, scheduler));
 	}
 
 	static final Consumer<Disposable> NOOP_DISCONNECT = runnable -> {
