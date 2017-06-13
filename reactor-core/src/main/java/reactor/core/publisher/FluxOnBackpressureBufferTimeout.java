b5484476000e4e7f51193fe5e1500c465081046d
Simon Baslé <sbasle@pivotal.io>
2017-06-13 17:04:01 +0200

    fix #296 Add a onBackpressureBuffer variant with a TTL
    This commit adds a onBackpressureBuffer variants that limits the backlog
    via both a maximum buffer size (dropping oldest elements when buffer
    fills up) and a TTL (dropping elements that have been buffered for
    longer than a specified duration).

---
+++ reactor-core/src/main/java/reactor/core/publisher/FluxOnBackpressureBufferTimeout.java
@@ -0,0 +1,363 @@
+/*
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.core.publisher;
+
+import java.time.Duration;
+import java.util.ArrayDeque;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.atomic.AtomicLongFieldUpdater;
+import java.util.function.Consumer;
+import javax.annotation.Nullable;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.scheduler.Scheduler;
+import reactor.util.Logger;
+import reactor.util.Loggers;
+
+/**
+ * Buffers values if the subscriber doesn't request fast enough, bounding the
+ * buffer to a chosen size and applying a TTL (time-to-live) to the elements.
+ * If the buffer overflows, drop the oldest element.
+ *
+ * @author Stephane Maldini
+ * @author Simon Baslé
+ * @author David Karnok
+ */
+//see https://github.com/akarnokd/RxJava2Extensions/blob/master/src/main/java/hu/akarnokd/rxjava2/operators/FlowableOnBackpressureTimeout.java
+final class FluxOnBackpressureBufferTimeout<O> extends FluxSource<O, O> {
+
+	private static final Logger LOGGER = Loggers.getLogger(FluxOnBackpressureBufferTimeout.class);
+
+	final Duration               ttl;
+	final Scheduler              ttlScheduler;
+	final int                    bufferSize;
+	final Consumer<? super O>    onBufferEviction;
+
+	FluxOnBackpressureBufferTimeout(Flux<? extends O> source,
+			Duration ttl, Scheduler ttlScheduler,
+			int bufferSize,
+			Consumer<? super O> onBufferEviction) {
+		super(source);
+		this.ttl = ttl;
+		this.ttlScheduler = ttlScheduler;
+		this.bufferSize = bufferSize;
+		this.onBufferEviction = onBufferEviction;
+	}
+
+	@Override
+	public void subscribe(Subscriber<? super O> s) {
+		source.subscribe(new BackpressureBufferTimeoutSubscriber<>(s,
+				ttl, ttlScheduler,
+				bufferSize,
+				onBufferEviction));
+	}
+
+	@Override
+	public int getPrefetch() {
+		return Integer.MAX_VALUE;
+	}
+
+	static final class BackpressureBufferTimeoutSubscriber<T> extends ArrayDeque<Object>
+			implements InnerOperator<T, T>, Runnable {
+
+		final Subscriber<? super T>  actual;
+		final Duration               ttl;
+		final Scheduler              ttlScheduler;
+		final Scheduler.Worker       worker;
+		final int                    bufferSizeDouble;
+		final Consumer<? super T>    onBufferEviction;
+
+		Subscription s;
+
+		volatile boolean cancelled;
+
+		volatile boolean done;
+		Throwable error;
+
+		volatile int wip;
+		static final AtomicIntegerFieldUpdater<BackpressureBufferTimeoutSubscriber> WIP = AtomicIntegerFieldUpdater.newUpdater(
+				BackpressureBufferTimeoutSubscriber.class,
+				"wip");
+
+		volatile long requested;
+		static final AtomicLongFieldUpdater<BackpressureBufferTimeoutSubscriber>
+				REQUESTED = AtomicLongFieldUpdater.newUpdater(
+				BackpressureBufferTimeoutSubscriber.class,
+				"requested");
+
+		BackpressureBufferTimeoutSubscriber(Subscriber<? super T> actual,
+				Duration ttl, Scheduler ttlScheduler,
+				int bufferSize,
+				Consumer<? super T> onBufferEviction) {
+			this.actual = actual;
+			this.onBufferEviction = Objects.requireNonNull(onBufferEviction, "buffer eviction callback must not be null");
+			this.bufferSizeDouble = bufferSize << 1;
+			this.ttl = ttl;
+			this.ttlScheduler = Objects.requireNonNull(ttlScheduler, "ttl Scheduler must not be null");
+			this.worker = ttlScheduler.createWorker();
+		}
+
+		@Override
+		@Nullable
+		public Object scanUnsafe(Attr key) {
+			if (key == ScannableAttr.PARENT) {
+				return s;
+			}
+			if (key == LongAttr.REQUESTED_FROM_DOWNSTREAM) {
+				return requested;
+			}
+			if (key == BooleanAttr.TERMINATED) {
+				return done && isEmpty();
+			}
+			if (key == BooleanAttr.CANCELLED) {
+				return cancelled;
+			}
+			if (key == IntAttr.BUFFERED) {
+				return size();
+			}
+			if (key == ThrowableAttr.ERROR) {
+				return error;
+			}
+			if (key == IntAttr.PREFETCH) {
+				return Integer.MAX_VALUE;
+			}
+			if (key == BooleanAttr.DELAY_ERROR) {
+				return false;
+			}
+
+			return InnerOperator.super.scanUnsafe(key);
+		}
+
+		@Override
+		public Subscriber<? super T> actual() {
+			return actual;
+		}
+
+		@Override
+		public void request(long n) {
+			if (Operators.validate(n)) {
+				Operators.getAndAddCap(REQUESTED, this, n);
+				drain();
+			}
+		}
+
+		@Override
+		public void cancel() {
+			cancelled = true;
+			s.cancel();
+			worker.dispose();
+
+			if (WIP.getAndIncrement(this) == 0) {
+				clearQueue();
+			}
+		}
+
+		@SuppressWarnings("unchecked")
+		void clearQueue() {
+			for (;;) {
+				T evicted;
+				synchronized (this) {
+					if (this.isEmpty()) {
+						break;
+					}
+
+					this.poll();
+					evicted = (T) this.poll();
+				}
+
+				evict(evicted);
+			}
+		}
+
+		@Override
+		public void onSubscribe(Subscription s) {
+			if (Operators.validate(this.s, s)) {
+				this.s = s;
+
+				actual.onSubscribe(this);
+
+				s.request(Long.MAX_VALUE);
+			}
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public void onNext(T t) {
+			T evicted = null;
+			synchronized (this) {
+				if (this.size() == bufferSizeDouble) {
+					this.poll();
+					evicted = (T) this.poll();
+				}
+				this.offer(ttlScheduler.now(TimeUnit.MILLISECONDS));
+				this.offer(t);
+			}
+			evict(evicted);
+			worker.schedule(this, ttl.toMillis(), TimeUnit.MILLISECONDS);
+			drain();
+		}
+
+		@Override
+		public void onError(Throwable t) {
+			error = t;
+			done = true;
+			drain();
+		}
+
+		@Override
+		public void onComplete() {
+			done = true;
+			drain();
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public void run() {
+			for (;;) {
+				if (cancelled) {
+					break;
+				}
+
+				boolean d = done;
+				boolean empty;
+				T evicted = null;
+
+				synchronized (this) {
+					Long ts = (Long) this.peek();
+					empty = ts == null;
+					if (!empty) {
+						if (ts <= ttlScheduler.now(TimeUnit.MILLISECONDS) - ttl.toMillis()) {
+							this.poll();
+							evicted = (T) this.poll();
+						} else {
+							break;
+						}
+					}
+				}
+
+				evict(evicted);
+
+				if (empty) {
+					if (d) {
+						drain();
+					}
+					break;
+				}
+			}
+		}
+
+		void evict(@Nullable T evicted) {
+			if (evicted != null) {
+				try {
+					onBufferEviction.accept(evicted);
+				} catch (Throwable ex) {
+					if (LOGGER.isDebugEnabled()) {
+						LOGGER.debug("value [{}] couldn't be evicted due to a callback error. This error will be dropped: {}", evicted, ex);
+					}
+					Operators.onErrorDropped(ex);
+				}
+			}
+		}
+
+		@SuppressWarnings("unchecked")
+		void drain() {
+			if (WIP.getAndIncrement(this) != 0) {
+				return;
+			}
+
+			int missed = 1;
+
+			for (;;) {
+				long r = requested;
+				long e = 0;
+
+				while (e != r) {
+					if (cancelled) {
+						clearQueue();
+						return;
+					}
+
+					boolean d = done;
+					T v;
+
+					synchronized (this) {
+						if (this.poll() != null) {
+							v = (T) this.poll();
+						} else {
+							v = null;
+						}
+					}
+
+					boolean empty = v == null;
+
+					if (d && empty) {
+						Throwable ex = error;
+						if (ex != null) {
+							actual.onError(ex);
+						} else {
+							actual.onComplete();
+						}
+
+						worker.dispose();
+						return;
+					}
+
+					if (empty) {
+						break;
+					}
+
+					actual.onNext(v);
+
+					e++;
+				}
+
+				if (e == r) {
+					if (cancelled) {
+						clearQueue();
+						return;
+					}
+
+					boolean d = done;
+					boolean empty;
+					synchronized (this) {
+						empty = this.isEmpty();
+					}
+
+					if (d && empty) {
+						Throwable ex = error;
+						if (ex != null) {
+							actual.onError(ex);
+						} else {
+							actual.onComplete();
+						}
+
+						worker.dispose();
+						return;
+					}
+				}
+
+				missed = WIP.addAndGet(this, -missed);
+				if (missed == 0) {
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ reactor-core/src/test/java/reactor/core/publisher/FluxOnBackpressureBufferTimeoutTest.java
@@ -0,0 +1,299 @@
+/*
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.core.publisher;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
+import org.junit.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+import reactor.test.publisher.TestPublisher;
+import reactor.test.scheduler.VirtualTimeScheduler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FluxOnBackpressureBufferTimeoutTest implements Consumer<Object> {
+
+	final List<Object> evicted = Collections.synchronizedList(new ArrayList<>());
+
+	@Override
+	public void accept(Object t) {
+		evicted.add(t);
+	}
+
+	@Test
+	public void empty() {
+		StepVerifier.create(Flux.empty().onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, v -> {}))
+		            .verifyComplete();
+	}
+
+	@Test
+	public void error() {
+		StepVerifier.create(Flux.error(new IOException())
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, v -> {}))
+		            .verifyError(IOException.class);
+	}
+
+	@Test
+	public void errorDelayed() {
+		StepVerifier.create(Flux.just(1)
+		                        .concatWith(Flux.error(new IOException()))
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, v -> {}),
+		0)
+		            .expectSubscription()
+		            .expectNoEvent(Duration.ofMillis(50))
+		            .thenRequest(1)
+		            .expectNext(1)
+		            .verifyError(IOException.class);
+	}
+
+	@Test
+	public void normal1() {
+		StepVerifier.create(Flux.range(1, 5)
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, v -> {}))
+		            .expectNext(1, 2, 3, 4, 5)
+		            .verifyComplete();
+	}
+
+	@Test
+	public void normal1SingleStep() {
+		StepVerifier.create(Flux.range(1, 5)
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, v -> {})
+		                        .limitRate(1))
+		            .expectNext(1, 2, 3, 4, 5)
+		            .verifyComplete();
+	}
+
+	@Test
+	public void normal2() {
+		StepVerifier.create(Flux.range(1, 5)
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, v -> {}, Schedulers.single()))
+		            .expectNext(1, 2, 3, 4, 5)
+		            .verifyComplete();
+	}
+
+	@Test
+	public void normal2SingleStep() {
+		StepVerifier.create(Flux.range(1, 5)
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, v -> {}, Schedulers.single())
+		                        .limitRate(1))
+		            .expectNext(1, 2, 3, 4, 5)
+		            .verifyComplete();
+	}
+
+	@Test
+	public void normal3() {
+		StepVerifier.create(Flux.range(1, 5)
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), 10, this, Schedulers.single()))
+		            .expectNext(1, 2, 3, 4, 5)
+		            .verifyComplete();
+	}
+
+	@Test
+	public void normal3SingleStep() {
+		StepVerifier.create(Flux.range(1, 5)
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), 10, this, Schedulers.single())
+		                        .limitRate(1))
+		            .expectNext(1, 2, 3, 4, 5)
+		            .verifyComplete();
+	}
+
+	@Test
+	public void normal4() {
+		StepVerifier.create(Flux.range(1, 5)
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, this, Schedulers.single()))
+		            .expectNext(1, 2, 3, 4, 5)
+		            .verifyComplete();
+	}
+
+	@Test
+	public void normal4SingleStep() {
+		StepVerifier.create(Flux.range(1, 5)
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, this, Schedulers.single())
+		                        .limitRate(1))
+		            .expectNext(1, 2, 3, 4, 5)
+		            .verifyComplete();
+	}
+
+	@Test
+	public void bufferLimit() {
+		StepVerifier.create(Flux.range(1, 5)
+				.onBackpressureBuffer(Duration.ofMinutes(1), 1, this, Schedulers.single()),
+				0)
+		            .expectSubscription()
+		            .expectNoEvent(Duration.ofMillis(100))
+		            .thenRequest(1)
+		            .expectNext(5)
+		            .verifyComplete();
+
+		assertThat(evicted).containsExactly(1, 2, 3, 4);
+	}
+
+	@Test
+	public void timeoutLimit() {
+		TestPublisher<Integer> tp = TestPublisher.create();
+		StepVerifier.withVirtualTime(() ->
+				tp.flux().onBackpressureBuffer(Duration.ofSeconds(1), 1, this, VirtualTimeScheduler.get()), 0)
+		            .expectSubscription()
+		            .then(() -> tp.next(1))
+		            .expectNoEvent(Duration.ofMillis(500))
+		            .then(() -> tp.next(2))
+		            .expectNoEvent(Duration.ofMillis(500))
+		            .then(() -> tp.next(3))
+		            .expectNoEvent(Duration.ofMillis(500))
+		            .then(() -> tp.next(4))
+		            .expectNoEvent(Duration.ofMillis(500))
+		            .then(() -> tp.next(5))
+		            .expectNoEvent(Duration.ofMillis(500))
+		            .then(tp::complete)
+		            .thenRequest(1)
+		            .expectNext(5)
+		            .verifyComplete();
+
+		assertThat(evicted).containsExactly(1, 2, 3, 4);
+	}
+
+	@Test
+	public void take() {
+		StepVerifier.create(Flux.range(1, 5)
+		                        .onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, v -> {})
+		                        .take(2))
+		            .expectNext(1, 2)
+		            .verifyComplete();
+	}
+
+	@Test
+	public void cancelEvictAll() {
+		StepVerifier.create(Flux.range(1, 5)
+		                        .log()
+				.onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, this,
+						Schedulers.single()),
+				0)
+		            .thenAwait(Duration.ofMillis(100)) //small hiccup to cancel after the prefetch
+		            .thenCancel()
+		            .verify();
+
+		assertThat(evicted).containsExactly(1, 2, 3, 4, 5);
+	}
+
+	@Test
+	public void timeoutEvictAll() {
+		StepVerifier.withVirtualTime(() -> Flux.range(1, 5)
+		                                       .onBackpressureBuffer(Duration.ofSeconds(1), Integer.MAX_VALUE, this, VirtualTimeScheduler.get()),
+				0)
+		            .expectSubscription()
+		            .expectNoEvent(Duration.ofSeconds(1))
+		            .thenAwait(Duration.ofMinutes(1))
+		            .thenRequest(1)
+		            .verifyComplete();
+
+		assertThat(evicted).containsExactly(1, 2, 3, 4, 5);
+	}
+
+	@Test
+	public void evictCancels() {
+		AtomicReference<Subscription> subscription = new AtomicReference<>();
+		TestPublisher<Integer> tp = TestPublisher.create();
+		StepVerifier.withVirtualTime(() -> tp.flux()
+		                                     .doOnSubscribe(subscription::set)
+		                                     .onBackpressureBuffer(Duration.ofSeconds(1), 10, i -> {
+			                                     evicted.add(i);
+			                                     subscription.get().cancel();
+		                                     }, VirtualTimeScheduler.get()),
+				0)
+		            .then(() -> tp.emit(1, 2, 3, 4, 5))
+		            .thenAwait(Duration.ofMinutes(1))
+		            .verifyComplete();
+
+		assertThat(evicted).containsExactly(1, 2, 3, 4, 5);
+	}
+
+	@Test
+	public void evictThrows() {
+
+		TestPublisher<Integer> tp = TestPublisher.create();
+		StepVerifier.withVirtualTime(() -> tp.flux()
+		                                     .onBackpressureBuffer(Duration.ofSeconds(1), 10, i -> {
+			                                     throw new IllegalStateException(i.toString());
+		                                     }),
+				0)
+		            .then(() -> tp.emit(1, 2, 3, 4, 5))
+		            .thenAwait(Duration.ofMinutes(1))
+		            .thenRequest(1)
+		            .expectComplete()
+		            .verifyThenAssertThat()
+		            .hasDroppedErrors(5)
+		            .hasDroppedErrorsSatisfying(c -> {
+			            Iterator<Throwable> it = c.iterator();
+			            for (int i = 1; it.hasNext(); i++) {
+			            	assertThat(it.next())
+						            .isInstanceOf(IllegalStateException.class)
+						            .hasMessage("" + i);
+			            }
+		            });
+	}
+
+	@Test
+	public void cancelAndRequest() {
+		List<Integer> seen = Collections.synchronizedList(new ArrayList<>());
+
+		Flux.range(1, 5)
+		    .onBackpressureBuffer(Duration.ofMinutes(1), Integer.MAX_VALUE, this, Schedulers.single())
+		    .subscribe(new BaseSubscriber<Integer>() {
+			    @Override
+			    protected void hookOnSubscribe(Subscription subscription) {
+				    request(1);
+			    }
+
+			    @Override
+			    protected void hookOnNext(Integer value) {
+			    	seen.add(value);
+				    cancel();
+			    }
+		    });
+
+		assertThat(seen).containsExactly(1);
+	}
+
+	@Test
+	public void dropBySizeAndTimeout() {
+		TestPublisher<Integer> tp = TestPublisher.create();
+
+		StepVerifier.withVirtualTime(() -> tp.flux()
+		                                     .onBackpressureBuffer(Duration.ofMillis(600),
+				                                     5, this).log(),
+				0)
+		            .expectSubscription()
+		            .then(() -> tp.next(1,2, 3, 4, 5, 6, 7)) //evict 2 elements
+		            .then(() -> assertThat(evicted).containsExactly(1, 2))
+		            .thenAwait(Duration.ofMillis(500))
+		            .then(() -> tp.emit(8, 9, 10))
+		            .thenAwait(Duration.ofMillis(100)) // evict elements older than 8
+		            .then(() -> assertThat(evicted).containsExactly(1, 2, 3, 4, 5, 6, 7))
+		            .thenRequest(10)
+		            .expectNext(8, 9, 10)
+		            .verifyComplete();
+	}
+}
