6a4700af247d1f48adcf40be5a8af868d4ef5a38
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-06-14 11:52:07 +0200

    Introduce Kotlin extensions

---
+++ reactor-core/src/test/kotlin/reactor/core/publisher/MonoExtensionsTests.kt
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.core.publisher
+
+import org.junit.Assert
+import org.junit.Test
+import reactor.test.StepVerifier
+import reactor.test.verifyError
+import java.io.IOException
+import java.util.concurrent.Callable
+import java.util.concurrent.CompletableFuture
+
+class MonoExtensionsTests {
+
+    @Test
+    fun anyToMono() {
+        StepVerifier
+                .create("foo".toMono())
+                .expectNext("foo")
+                .verifyComplete()
+    }
+
+    @Test
+    fun completableFutureToMono() {
+        var future = CompletableFuture<String>()
+
+        var verifier = StepVerifier.create(future.toMono())
+                .expectNext("foo")
+                .expectComplete()
+        future.complete("foo")
+        verifier.verify()
+    }
+
+    @Test
+    fun callableToMono() {
+        val callable = Callable { "foo" }
+        var verifier = StepVerifier.create(callable.toMono())
+                .expectNext("foo")
+                .expectComplete()
+        verifier.verify()
+    }
+
+    @Test
+    fun throwableToMono() {
+        StepVerifier.create(IllegalStateException()
+                .toMono<Any>())
+                .verifyError(IllegalStateException::class)
+    }
+
+    @Test
+    fun `cast() with generic parameter`() {
+        val monoOfAny: Mono<Any> = Mono.just("foo")
+        StepVerifier
+                .create(monoOfAny.cast<String>())
+                .expectNext("foo")
+                .verifyComplete()
+    }
+
+    @Test
+    fun doOnError() {
+        val monoOnError: Mono<Any> = IllegalStateException().toMono()
+        var invoked = false
+        monoOnError.doOnError(IllegalStateException::class, {
+            invoked = true
+        }).subscribe()
+        Assert.assertTrue(invoked)
+    }
+
+    @Test
+    fun onErrorMap() {
+        StepVerifier.create(IOException()
+                .toMono<Any>()
+                .onErrorMap(IOException::class, ::IllegalStateException))
+                .verifyError<IllegalStateException>()
+    }
+
+    @Test
+    fun `ofType() with generic parameter`() {
+        StepVerifier.create("foo".toMono().ofType<String>()).expectNext("foo").verifyComplete()
+        StepVerifier.create("foo".toMono().ofType<Int>()).verifyComplete()
+    }
+
+    @Test
+    fun onErrorResume() {
+        StepVerifier.create(IOException()
+                .toMono<String>()
+                .onErrorResume(IOException::class, { "foo".toMono() }))
+                .expectNext("foo")
+                .verifyComplete()
+    }
+
+    @Test
+    fun onErrorReturn() {
+        StepVerifier.create(IOException()
+                .toMono<String>()
+                .onErrorReturn(IOException::class, "foo"))
+                .expectNext("foo")
+                .verifyComplete()
+    }
+
+}
---
+++ reactor-core/src/test/kotlin/reactor/core/publisher/MonoWhenFunctionsTests.kt
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.core.publisher
+
+import org.junit.Test
+import reactor.test.StepVerifier
+import reactor.test.publisher.TestPublisher
+import reactor.util.function.Tuples
+
+class MonoWhenFunctionsTests {
+
+    @Test
+    fun `whenComplete with two Monos`() {
+        StepVerifier.create(whenComplete("foo1".toMono(), "foo2".toMono()))
+                .expectNext(Tuples.of("foo1", "foo2"))
+                .verifyComplete()
+    }
+
+    @Test
+    fun `whenComplete with two Monos and combinator`() {
+        StepVerifier.create(whenComplete("foo1".toMono(), "foo2".toMono()) { a, b -> Pair(a, b) })
+                .expectNext(Pair("foo1", "foo2"))
+                .verifyComplete()
+    }
+
+    @Test
+    fun `whenComplete with three Monos`() {
+        StepVerifier.create(whenComplete("foo1".toMono(), "foo2".toMono(), "foo3".toMono()))
+                .expectNext(Tuples.of("foo1", "foo2", "foo3"))
+                .verifyComplete()
+    }
+
+    @Test
+    fun `whenComplete with four Monos`() {
+        StepVerifier.create(whenComplete("foo1".toMono(), "foo2".toMono(), "foo3".toMono(), "foo4".toMono()))
+                .expectNext(Tuples.of("foo1", "foo2", "foo3", "foo4"))
+                .verifyComplete()
+    }
+
+    @Test
+    fun `whenComplete with five Monos`() {
+        StepVerifier.create(whenComplete("foo1".toMono(), "foo2".toMono(), "foo3".toMono(), "foo4".toMono(), "foo5".toMono()))
+                .expectNext(Tuples.of("foo1", "foo2", "foo3", "foo4", "foo5"))
+                .verifyComplete()
+    }
+
+    @Test
+    fun `whenComplete with six Monos`() {
+        StepVerifier.create(whenComplete("foo1".toMono(), "foo2".toMono(), "foo3".toMono(),
+                "foo4".toMono(), "foo5".toMono(), "foo6".toMono()))
+                .expectNext(Tuples.of("foo1", "foo2", "foo3", "foo4", "foo5", "foo6"))
+                .verifyComplete()
+    }
+
+    @Test
+    fun `whenComplete with an Iterable of Mono + and a combinator`() {
+        StepVerifier.create(listOf("foo1".toMono(), "foo2".toMono(), "foo3".toMono())
+                .whenComplete { it.reduce { acc, s -> acc + s }})
+                .expectNext("foo1foo2foo3")
+                .verifyComplete()
+    }
+
+    @Test
+    fun `whenComplete on an Iterable of void Publishers`() {
+        val publishers = Array(3, { TestPublisher.create<Void>() })
+        publishers.forEach { it.complete() }
+        StepVerifier.create(publishers.asIterable().whenComplete())
+                .verifyComplete()
+    }
+
+    @Test
+    fun `whenComplete on an Iterable of Monos with combinator`() {
+        StepVerifier.create(listOf("foo1", "foo2", "foo3").map { it.toMono() }.whenComplete { it.joinToString() })
+                .expectNext("foo1, foo2, foo3")
+                .verifyComplete()
+    }
+
+    @Test
+    fun `whenComplete with void Publishers`() {
+        val publishers = Array(3, { TestPublisher.create<Void>() })
+        publishers.forEach { it.complete() }
+        StepVerifier.create(whenComplete(*publishers))
+                .verifyComplete()
+    }
+
+    @Test
+    fun `whenComplete with Monos and combinator`() {
+        StepVerifier.create(whenComplete("foo1".toMono(), "foo2".toMono(), "foo3".toMono()) { it.joinToString() })
+                .expectNext("foo1, foo2, foo3")
+                .verifyComplete()
+    }
+
+}
