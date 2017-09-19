53fb049a4d0d4949475f2d44d7a527b722f3bbf2
Stephane Maldini <smaldini@pivotal.io>
2017-09-18 22:07:51 -0700

    fix #389 - support conditional subscriber in parallelflux

--- reactor-core/src/test/java/reactor/core/publisher/ParallelFilterTest.java
+++ reactor-core/src/test/java/reactor/core/publisher/ParallelFilterTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,6 +18,8 @@ package reactor.core.publisher;
 
 import org.junit.Test;
 import reactor.core.Scannable;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,6 +44,21 @@ public class ParallelFilterTest {
 		assertThat(test.scan(Scannable.Attr.PREFETCH))
 				.isEqualTo(-1)
 				.isNotEqualTo(source.getPrefetch());
+	}
+
+	@Test
+	public void conditional() {
+		Flux<Integer> source = Flux.range(1, 1_000);
+		for (int i = 1; i < 33; i++) {
+			Flux<Integer> result = ParallelFlux.from(source, i)
+			                                   .filter(t -> true)
+			                                   .filter(t -> true)
+			                                   .sequential();
+
+			StepVerifier.create(result)
+			            .expectNextCount(1_000)
+			            .verifyComplete();
+		}
 	}
 
 }
