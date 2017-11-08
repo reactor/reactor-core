abefb00a5ae610e128fceb7c6933ad559f7d81e3
Simon Baslé <sbasle@pivotal.io>
2017-11-08 15:41:21 +0100

    conditional implementation for FluxIndexed

--- reactor-core/src/main/java/reactor/core/publisher/FluxIndexed.java
+++ reactor-core/src/main/java/reactor/core/publisher/FluxIndexed.java
@@ -21,18 +21,25 @@ import java.util.function.BiFunction;
 
 import org.reactivestreams.Subscription;
 import reactor.core.CoreSubscriber;
+import reactor.core.Fuseable.ConditionalSubscriber;
 import reactor.util.annotation.Nullable;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
 
 /**
+ * An operator that tags the values it passes through with their index in the original
+ * sequence, either as their natural long index (0-based) or as a customized index
+ * by way of a user-provided {@link BiFunction}. The resulting sequence is one of
+ * {@link Tuple2}, t1 value being the index (as mapped by the user function) and t2 the
+ * source value.
+ *
  * @author Simon Baslé
  */
 public class FluxIndexed<T, I> extends FluxOperator<T, Tuple2<I, T>> {
 
 	private final BiFunction<? super Long, ? super T, ? extends I> indexMapper;
 
-	public FluxIndexed(Flux<T> source,
+	FluxIndexed(Flux<T> source,
 			BiFunction<? super Long, ? super T, ? extends I> indexMapper) {
 		super(source);
 		this.indexMapper = Objects.requireNonNull(indexMapper, "indexMapper must be non null");
@@ -40,10 +47,17 @@ public class FluxIndexed<T, I> extends FluxOperator<T, Tuple2<I, T>> {
 
 	@Override
 	public void subscribe(CoreSubscriber<? super Tuple2<I, T>> actual) {
-		source.subscribe(new FluxIndexedSubscriber<>(actual, indexMapper));
+		if (actual instanceof ConditionalSubscriber) {
+			@SuppressWarnings("unchecked") ConditionalSubscriber<? super Tuple2<I, T>> cs =
+					(ConditionalSubscriber<? super Tuple2<I, T>>) actual;
+			source.subscribe(new IndexedConditionalSubscriber<>(cs, indexMapper));
+		}
+		else {
+			source.subscribe(new IndexedSubscriber<>(actual, indexMapper));
+		}
 	}
 
-	static final class FluxIndexedSubscriber<I, T> implements InnerOperator<T, Tuple2<I, T>> {
+	static final class IndexedSubscriber<I, T> implements InnerOperator<T, Tuple2<I, T>> {
 
 		final CoreSubscriber<? super Tuple2<I, T>>             actual;
 		final BiFunction<? super Long, ? super T, ? extends I> indexMapper;
@@ -52,7 +66,7 @@ public class FluxIndexed<T, I> extends FluxOperator<T, Tuple2<I, T>> {
 		Subscription s;
 		long         index = 0;
 
-		public FluxIndexedSubscriber(CoreSubscriber<? super Tuple2<I, T>> actual,
+		IndexedSubscriber(CoreSubscriber<? super Tuple2<I, T>> actual,
 				BiFunction<? super Long, ? super T, ? extends I> indexMapper) {
 			this.actual = actual;
 			this.indexMapper = indexMapper;
@@ -97,6 +111,121 @@ public class FluxIndexed<T, I> extends FluxOperator<T, Tuple2<I, T>> {
 			done = true;
 
 			actual.onError(t);
+		}
+
+		@Override
+		public void onComplete() {
+			if (done) {
+				return;
+			}
+			done = true;
+
+			actual.onComplete();
+		}
+
+		@Override
+		public CoreSubscriber<? super Tuple2<I, T>> actual() {
+			return this.actual;
+		}
+
+		@Override
+		public void request(long n) {
+			s.request(n);
+		}
+
+		@Override
+		public void cancel() {
+			s.cancel();
+		}
+
+		@Override
+		@Nullable
+		public Object scanUnsafe(Attr key) {
+			if (key == Attr.PARENT) return s;
+			if (key == Attr.TERMINATED) return done;
+
+			return InnerOperator.super.scanUnsafe(key);
+		}
+	}
+
+	static final class IndexedConditionalSubscriber<I, T> implements InnerOperator<T, Tuple2<I, T>>,
+	                                                                 ConditionalSubscriber<T> {
+
+		final ConditionalSubscriber<? super Tuple2<I, T>>      actual;
+		final BiFunction<? super Long, ? super T, ? extends I> indexMapper;
+
+		Subscription s;
+		boolean done;
+		long index;
+
+		IndexedConditionalSubscriber(
+				ConditionalSubscriber<? super Tuple2<I,T>> cs,
+				BiFunction<? super Long, ? super T, ? extends I> indexMapper) {
+			this.actual = cs;
+			this.indexMapper = indexMapper;
+		}
+
+		@Override
+		public void onSubscribe(Subscription s) {
+			if (Operators.validate(this.s, s)) {
+				this.s = s;
+				actual.onSubscribe(this);
+			}
+		}
+
+		@Override
+		public boolean tryOnNext(T t) {
+			if (done) {
+				Operators.onNextDropped(t, actual.currentContext());
+				return true;
+			}
+
+			I typedIndex;
+			long i = this.index;
+			try {
+				typedIndex = Objects.requireNonNull(indexMapper.apply(i, t),
+						"indexMapper returned a null value at raw index " + i +
+								" for value " + t);
+				this.index = i + 1L;
+			}
+			catch (Throwable e) {
+				onError(Operators.onOperatorError(s, e, t, actual.currentContext()));
+				return true;
+			}
+
+			return actual.tryOnNext(Tuples.of(typedIndex, t));
+		}
+
+		@Override
+		public void onNext(T t) {
+			if (done) {
+				Operators.onNextDropped(t, actual.currentContext());
+				return;
+			}
+
+			long i = this.index;
+			try {
+				I typedIndex = Objects.requireNonNull(indexMapper.apply(i, t),
+						"indexMapper returned a null value at raw index " + i +
+								" for value " + t);
+				this.index = i + 1L;
+				actual.onNext(Tuples.of(typedIndex, t));
+			}
+			catch (Throwable e) {
+				onError(Operators.onOperatorError(s, e, t, actual.currentContext()));
+			}
+		}
+
+		@Override
+		public void onError(Throwable throwable) {
+			if (done) {
+				Operators.onErrorDropped(throwable, actual.currentContext());
+				return;
+			}
+
+			done = true;
+
+			actual.onError(throwable);
 		}
 
 		@Override
