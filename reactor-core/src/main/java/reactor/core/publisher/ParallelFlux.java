da58c9bac8aef25900b6d418f1e2db70109c30bb
Simon Baslé <sbasle@pivotal.io>
2018-02-26 11:17:38 +0100

    fix wording in ParallelFlux (not only cancel)

--- reactor-core/src/main/java/reactor/core/publisher/ParallelFlux.java
+++ reactor-core/src/main/java/reactor/core/publisher/ParallelFlux.java
@@ -430,9 +430,9 @@ public abstract class ParallelFlux<T> implements Publisher<T> {
 	 * Call the specified callback when a 'rail' receives a Subscription from its
 	 * upstream.
 	 * <p>
-	 * This method is <strong>not</strong> intended for capturing the subscription and calling {@link Subscription#cancel()}.
-	 * The correct way to cancel a subscription is to call {@link Disposable#dispose()}
-	 * on the Disposable returned by {@link ParallelFlux#subscribe()}.
+	 * This method is <strong>not</strong> intended for capturing the subscription and calling its methods,
+	 * but for side effects like monitoring. For instance, the correct way to cancel a subscription is
+	 * to call {@link Disposable#dispose()} on the Disposable returned by {@link ParallelFlux#subscribe()}.
 	 *
 	 * @param onSubscribe the callback
 	 *
