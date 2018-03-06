ad3d61d35db551b57b13cc6494fddbfa02b63d69
Simon Baslé <sbasle@pivotal.io>
2018-03-06 13:39:42 +0100

    fix #1100 Add a error(Supplier) variant to Flux and Mono
    This variant helps avoiding eager creation of the `Throwable`, which
    can be a bottleneck (with the stacktrace filling) in certain configurations
    where Mono/Flux.error is used a lot.

---
+++ reactor-core/src/main/java/reactor/core/publisher/MonoErrorSupplied.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package reactor.core.publisher;
+
+import java.time.Duration;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+import reactor.core.CoreSubscriber;
+import reactor.core.Exceptions;
+import reactor.core.Fuseable;
+
+/**
+ * Emits a lazily generated {@link Throwable} instance to Subscribers, via a {@link Supplier}.
+ *
+ * @param <T> the value type
+ * @see <a href="https://github.com/reactor/reactive-streams-commons">Reactive-Streams-Commons</a>
+ */
+final class MonoErrorSupplied<T> extends Mono<T> implements Fuseable.ScalarCallable{
+
+	final Supplier<Throwable> errorSupplier;
+
+	MonoErrorSupplied(Supplier<Throwable> errorSupplier) {
+		this.errorSupplier = Objects.requireNonNull(errorSupplier, "errorSupplier");
+	}
+
+	@Override
+	public T block(Duration m) {
+		Throwable error = Objects.requireNonNull(errorSupplier.get(), "the errorSupplier returned null");
+		throw Exceptions.propagate(error);
+	}
+
+	@Override
+	public T block() {
+		Throwable error = Objects.requireNonNull(errorSupplier.get(), "the errorSupplier returned null");
+		throw Exceptions.propagate(error);
+	}
+
+	@Override
+	public void subscribe(CoreSubscriber<? super T> actual) {
+		Throwable error = Objects.requireNonNull(errorSupplier.get(), "the errorSupplier returned null");
+		Operators.error(actual, Operators.onOperatorError(error, actual.currentContext()));
+	}
+
+	@Override
+	public Object call() throws Exception {
+		Throwable error = Objects.requireNonNull(errorSupplier.get(), "the errorSupplier returned null");
+		if(error instanceof Exception){
+			throw ((Exception) error);
+		}
+		throw Exceptions.propagate(error);
+	}
+}
---
+++ reactor-core/src/test/java/reactor/core/publisher/MonoErrorSuppliedTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.core.publisher;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class MonoErrorSuppliedTest {
+
+	@Test
+	public void normal() {
+		StepVerifier.create(Mono.error(() -> new Exception("test")))
+		            .verifyErrorMessage("test");
+	}
+
+	@Test
+	public void throwOnBlock() {
+		assertThatExceptionOfType(IllegalStateException.class)
+				.isThrownBy(() -> new MonoErrorSupplied<>(() -> new IllegalStateException("boom"))
+						.block()
+				)
+				.withMessage("boom");
+	}
+
+	@Test
+	public void throwOnTimeoutBlock() {
+		assertThatExceptionOfType(IllegalStateException.class)
+				.isThrownBy(() -> new MonoErrorSupplied<>(() -> new IllegalStateException("boom"))
+						.block(Duration.ofMillis(100))
+				)
+				.withMessage("boom");
+	}
+
+	@Test
+	public void throwOnCall() {
+		assertThatExceptionOfType(IllegalStateException.class)
+				.isThrownBy(() -> new MonoErrorSupplied<>(() -> new IllegalStateException("boom"))
+						.call()
+				)
+				.withMessage("boom");
+	}
+
+	@Test
+	public void lazilyEvaluatedSubscribe() {
+		AtomicInteger count = new AtomicInteger();
+		Mono<Object> error = Mono.error(() -> new IllegalStateException("boom" + count.incrementAndGet()));
+
+		assertThat(count).as("no op before subscribe").hasValue(0);
+
+		StepVerifier.create(error.retry(3))
+		            .verifyErrorMessage("boom4");
+	}
+
+	@Test
+	public void lazilyEvaluatedBlock() {
+		AtomicInteger count = new AtomicInteger();
+		Mono<Object> error = Mono.error(() -> new IllegalStateException("boom" + count.incrementAndGet()));
+
+		assertThat(count).as("no op before block").hasValue(0);
+
+		assertThatExceptionOfType(IllegalStateException.class)
+				.isThrownBy(error::block)
+				.withMessage("boom1");
+
+		assertThat(count).as("after block").hasValue(1);
+	}
+
+	@Test
+	public void lazilyEvaluatedBlockTimeout() {
+		AtomicInteger count = new AtomicInteger();
+		Mono<Object> error = Mono.error(() -> new IllegalStateException("boom" + count.incrementAndGet()));
+
+		assertThat(count).as("no op before block").hasValue(0);
+
+		assertThatExceptionOfType(IllegalStateException.class)
+				.isThrownBy(() -> error.block(Duration.ofMillis(100)))
+				.withMessage("boom1");
+
+		assertThat(count).as("after block").hasValue(1);
+	}
+
+	@Test
+	public void lazilyEvaluatedCall() {
+		AtomicInteger count = new AtomicInteger();
+		MonoErrorSupplied<Object> error = new MonoErrorSupplied<>(() -> new IllegalStateException("boom" + count.incrementAndGet()));
+
+		assertThat(count).as("no op before call").hasValue(0);
+
+		assertThatExceptionOfType(IllegalStateException.class)
+				.isThrownBy(error::call)
+				.withMessage("boom1");
+
+		assertThat(count).as("after call").hasValue(1);
+	}
+}
