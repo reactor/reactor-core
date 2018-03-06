8b2a566b4418c1e2f6ae866a9b3d4d953908623e
Simon Baslé <sbasle@pivotal.io>
2018-03-06 11:07:17 +0100

    add the Flux equivalent

---
+++ reactor-core/src/main/java/reactor/core/publisher/FluxErrorSupplied.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright (c) 2011-2017 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.core.publisher;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+import reactor.core.CoreSubscriber;
+import reactor.core.Exceptions;
+import reactor.core.Fuseable;
+
+/**
+ * Emits a generated {@link Throwable} instance to Subscribers, lazily generated via a
+ * provided {@link java.util.function.Supplier}.
+ *
+ * @param <T> the value type
+ *
+ * @see <a href="https://github.com/reactor/reactive-streams-commons">Reactive-Streams-Commons</a>
+ */
+final class FluxErrorSupplied<T> extends Flux<T> implements Fuseable.ScalarCallable {
+
+	final Supplier<Throwable> errorSupplier;
+
+	FluxErrorSupplied(Supplier<Throwable> errorSupplier) {
+		this.errorSupplier = Objects.requireNonNull(errorSupplier, "errorSupplier");
+	}
+
+	@Override
+	public void subscribe(CoreSubscriber<? super T> actual) {
+		Throwable error = Objects.requireNonNull(errorSupplier.get(), "errorSupplier produced a null Throwable");
+		Operators.error(actual, Operators.onOperatorError(error, actual.currentContext()));
+	}
+
+	@Override
+	public Object call() throws Exception {
+		Throwable error = Objects.requireNonNull(errorSupplier.get(), "errorSupplier produced a null Throwable");
+		if(error instanceof Exception){
+			throw ((Exception)error);
+		}
+		throw Exceptions.propagate(error);
+	}
+}
---
+++ reactor-core/src/test/java/reactor/core/publisher/FluxErrorSuppliedTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.core.publisher;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class FluxErrorSuppliedTest {
+
+	@Test
+	public void normal() {
+		StepVerifier.create(Flux.error(() -> new Exception("test")))
+		            .verifyErrorMessage("test");
+	}
+
+	@Test
+	public void throwOnCall() {
+		assertThatExceptionOfType(IllegalStateException.class)
+				.isThrownBy(() -> new FluxErrorSupplied<>(() -> new IllegalStateException("boom"))
+						.call()
+				)
+				.withMessage("boom");
+	}
+
+	@Test
+	public void lazilyEvaluatedSubscribe() {
+		AtomicInteger count = new AtomicInteger();
+		Flux<Object> error = Flux.error(() -> new IllegalStateException("boom" + count.incrementAndGet()));
+
+		assertThat(count).as("no op before subscribe").hasValue(0);
+
+		StepVerifier.create(error.retry(3))
+		            .verifyErrorMessage("boom4");
+	}
+
+	@Test
+	public void lazilyEvaluatedCall() {
+		AtomicInteger count = new AtomicInteger();
+		FluxErrorSupplied<Object> error = new FluxErrorSupplied<>(() -> new IllegalStateException("boom" + count.incrementAndGet()));
+
+		assertThat(count).as("no op before call").hasValue(0);
+
+		assertThatExceptionOfType(IllegalStateException.class)
+				.isThrownBy(error::call)
+				.withMessage("boom1");
+
+		assertThat(count).as("after call").hasValue(1);
+	}
+}
