62f880ce4bcadf46ae70783cba6e14b90622e764
Simon Baslé <sbasle@pivotal.io>
2018-03-23 17:28:29 +0100

    add Traces javadoc

--- reactor-core/src/main/java/reactor/util/debug/Traces.java
+++ reactor-core/src/main/java/reactor/util/debug/Traces.java
@@ -27,6 +27,14 @@ import java.util.stream.Stream;
  */
 public class Traces {
 
+	/**
+	 * Transform the {@link StackTraceElement} array from an exception into a {@link String}
+	 * representation, each element being prepended with a tabulation and appended with a
+	 * newline. No sanitation is performed.
+	 *
+	 * @param stackTraceElements the array of {@link StackTraceElement} to convert to {@link String}
+	 * @return the string version of the stacktrace.
+	 */
 	public static String stackTraceToString(StackTraceElement[] stackTraceElements) {
 		StringBuilder sb = new StringBuilder();
 		for (StackTraceElement e : stackTraceElements) {
@@ -40,6 +48,14 @@ public class Traces {
 
 	}
 
+	/**
+	 * Transform the {@link StackTraceElement} array from an exception into a {@link String}
+	 * representation, each element being prepended with a tabulation and appended with a
+	 * newline, unless they don't pass the {@link #shouldSanitize(String) sanitation filter}.
+	 *
+	 * @param stackTraceElements the array of {@link StackTraceElement} to convert to {@link String}
+	 * @return the string version of the stacktrace.
+	 */
 	public static String stackTraceToSanitizedString(StackTraceElement[] stackTraceElements) {
 		StringBuilder sb = new StringBuilder();
 		for (StackTraceElement e : stackTraceElements) {
@@ -146,10 +162,53 @@ public class Traces {
 		return stripped;
 	}
 
+	/**
+	 * Extract operator information out of an assembly stack trace in {@link String} form
+	 * (see {@link #stackTraceToSanitizedString(StackTraceElement[])}).
+	 * <p>
+	 * Most operators will result in a line of the form {@code "Flux.map ⇢ user.code.Class.method(Class.java:123)"},
+	 * that is:
+	 * <ol>
+	 *     <li>The top of the stack is inspected for Reactor API references, and the deepest
+	 *     one is kept, since multiple API references generally denote an alias operator.
+	 *     (eg. {@code "Flux.map"})</li>
+	 *     <li>The next stacktrace element is considered user code and is appended to the
+	 *     result with a {@code ⇢} separator. (eg. {@code " ⇢ user.code.Class.method(Class.java:123)"})</li>
+	 *     <li>If no user code is found in the sanitized stack, then the API reference is outputed in the later format only.</li>
+	 *     <li>If the sanitized stack is empty, returns {@code "[no operator assembly information]"}</li>
+	 * </ol>
+	 *
+	 *
+	 * @param source the sanitized assembly stacktrace in String format.
+	 * @return a {@link String} representing operator and operator assembly site extracted
+	 * from the assembly stack trace.
+	 */
 	public static String extractOperatorAssemblyInformation(String source) {
 		return extractOperatorAssemblyInformation(source, false);
 	}
 
+	/**
+	 * Extract operator information out of an assembly stack trace in {@link String} form
+	 * (see {@link #stackTraceToSanitizedString(StackTraceElement[])}) which potentially
+	 * has a header line that one can skip by setting {@code skipFirst} to {@code true}.
+	 * <p>
+	 * Most operators will result in a line of the form {@code "Flux.map ⇢ user.code.Class.method(Class.java:123)"},
+	 * that is:
+	 * <ol>
+	 *     <li>The top of the stack is inspected for Reactor API references, and the deepest
+	 *     one is kept, since multiple API references generally denote an alias operator.
+	 *     (eg. {@code "Flux.map"})</li>
+	 *     <li>The next stacktrace element is considered user code and is appended to the
+	 *     result with a {@code ⇢} separator. (eg. {@code " ⇢ user.code.Class.method(Class.java:123)"})</li>
+	 *     <li>If no user code is found in the sanitized stack, then the API reference is outputed in the later format only.</li>
+	 *     <li>If the sanitized stack is empty, returns {@code "[no operator assembly information]"}</li>
+	 * </ol>
+	 *
+	 *
+	 * @param source the sanitized assembly stacktrace in String format.
+	 * @return a {@link String} representing operator and operator assembly site extracted
+	 * from the assembly stack trace.
+	 */
 	public static String extractOperatorAssemblyInformation(String source, boolean skipFirst) {
 		String[] uncleanTraces = source.split("\n");
 		final List<String> traces = Stream.of(uncleanTraces)
@@ -185,7 +244,7 @@ public class Traces {
 			userCodeLine = traces.get(i);
 		}
 
-		//now we want something in the form "Flux.map in user.code.Class.method(Class.java:123)"
+		//now we want something in the form "Flux.map ⇢ user.code.Class.method(Class.java:123)"
 		if (apiLine.isEmpty()) return userCodeLine;
 
 		int linePartIndex = apiLine.indexOf('(');
