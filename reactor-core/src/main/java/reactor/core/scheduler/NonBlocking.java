51dff9f255e6d7ca2fefaf23cb2fb2b9d9ddfdbf
Stephane Maldini <smaldini@pivotal.io>
2018-05-02 14:01:26 -0700

    Tweak NonBlocking Javadoc

--- reactor-core/src/main/java/reactor/core/scheduler/NonBlocking.java
+++ reactor-core/src/main/java/reactor/core/scheduler/NonBlocking.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,18 +16,12 @@
 
 package reactor.core.scheduler;
 
-import java.util.concurrent.ThreadFactory;
-
 /**
  * A marker interface that is detected on {@link Thread Threads} while executing Reactor
  * blocking APIs, resulting in these calls throwing an exception.
  * <p>
- * Extend {@link AbstractReactorThreadFactory} for a {@link ThreadFactory} that can easily
- * create such threads, and optionally name them and further configure them.
- * <p>
- * See {@link Schedulers#isBlockingCurrentThreadOk()} and
- * {@link Schedulers#isBlockingCurrentThreadOk(Thread)} for a check that includes detecting
- * this marker interface.
+ * See {@link Schedulers#isInNonBlockingThread()}} and
+ * {@link Schedulers#isNonBlockingThread(Thread)}
  *
  * @author Simon Baslé
  */
