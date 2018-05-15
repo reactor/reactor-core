c9406f34e246bff6c475d7ea709bf736ec5a2fae
Simon Baslé <sbasle@pivotal.io>
2018-05-15 20:33:00 +0200

    port #1204 Lift properly maintain GroupedFlux/ConnectableFlux interface
    This commit prevents potential ClassCastExceptions when lifting either a
    GroupedFlux or a ConnectableFlux, by instantiating dedicated operators
    similarly to what is done for ParallelFlux for example.
    
    This is a backport of commit f0b14c2, as tracked in #1198

---
+++ reactor-core/src/main/java/reactor/core/publisher/ConnectableLift.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.core.publisher;
+
+import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+import reactor.core.CoreSubscriber;
+import reactor.core.Disposable;
+import reactor.core.Scannable;
+import reactor.util.annotation.Nullable;
+
+/**
+ * @author Simon Baslé
+ */
+final class ConnectableLift<I, O> extends ConnectableFlux<O> implements Scannable {
+
+	final BiFunction<Scannable, ? super CoreSubscriber<? super O>, ? extends CoreSubscriber<? super I>>
+			lifter;
+
+	final ConnectableFlux<I> source;
+
+	ConnectableLift(ConnectableFlux<I> p,
+			BiFunction<Scannable, ? super CoreSubscriber<? super O>, ? extends CoreSubscriber<? super I>> lifter) {
+		this.source = Objects.requireNonNull(p, "source");
+		this.lifter = lifter;
+	}
+
+	@Override
+	public int getPrefetch() {
+		return source.getPrefetch();
+	}
+
+	@Override
+	public void connect(Consumer<? super Disposable> cancelSupport) {
+		this.source.connect();
+	}
+
+	@Override
+	@Nullable
+	public Object scanUnsafe(Attr key) {
+		if (key == Attr.PREFETCH) return source.getPrefetch();
+		if (key == Attr.PARENT) return source;
+		return null;
+	}
+
+	@Override
+	public void subscribe(CoreSubscriber<? super O> actual) {
+		CoreSubscriber<? super I> input =
+				lifter.apply(Scannable.from(source), actual);
+
+		Objects.requireNonNull(input, "Lifted subscriber MUST NOT be null");
+
+		source.subscribe(input);
+	}
+}
