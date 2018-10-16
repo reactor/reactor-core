a4009c45e9ecf265eacb22f6ed2fc1e5d3dda1e6
Simon Baslé <sbasle@pivotal.io>
2018-10-16 12:00:42 +0200

    in TDD spirit, prepare build and tests for "no micrometer" tests
    these tests should fail on CI, making #1383 obvious

---
+++ reactor-core/src/test/java/reactor/core/MetricsNoMicrometerTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.core;
+
+import org.junit.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+/**
+ * This test case should be OK to run in the normal test source set, but is intended
+ * for a test sourceset that doesn't include Micrometer dependency, and which only runs
+ * tests validating the likes of {@link Flux#metrics()} are NO-OP in that context.
+ *
+ * @author Simon Baslé
+ */
+public class MetricsNoMicrometerTest {
+
+	@Test
+	public void FluxMetricsNoOp() {
+		assertThatCode(() -> Flux.just("foo").hide().metrics().blockLast())
+				.doesNotThrowAnyException();
+	}
+
+	@Test
+	public void FluxMetricsFusedNoOp() {
+		assertThatCode(() -> Flux.just("foo").metrics().blockLast())
+				.doesNotThrowAnyException();
+	}
+
+	@Test
+	public void MonoMetricsNoOp() {
+		assertThatCode(() -> Mono.just("foo").hide().metrics().block())
+				.doesNotThrowAnyException();
+	}
+
+	@Test
+	public void MonoMetricsFusedNoOp() {
+		assertThatCode(() -> Mono.just("foo").metrics().block())
+				.doesNotThrowAnyException();
+	}
+
+}
