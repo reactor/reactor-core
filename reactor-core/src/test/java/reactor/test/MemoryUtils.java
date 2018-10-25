74177c587f7ef0b2163bba7f0f00f5c712a6dee1
Simon Baslé <sbasle@pivotal.io>
2018-10-25 19:22:28 +0200

    Avoid finalize in test, add MemoryUtils, alt build on JDK11 (#1407)
    GC1 pause tuning to attempt to prevent GC-related tests failing
    
    Bump to Gradle 4.10.2 and bump tools for Java 11
     - Mockito
     - Jacoco
     - AssertJ
     - JMH plugin
    
    Travis build on JDK11 instead of 9
    
    Avoid using finalize() in tests but use a new util that
    uses PhantomReferences.

---
+++ reactor-core/src/test/java/reactor/test/MemoryUtils.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package reactor.test;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Test utility around memory, references, leaks and retained object detection.
+ *
+ * @author Simon Baslé
+ */
+public class MemoryUtils {
+
+	/**
+	 * A utility class to check that some tracked object are finalized, by way of tracking
+	 * them through a {@link PhantomReference}.
+	 */
+	public static final class RetainedDetector {
+
+		private final ReferenceQueue<Object>         referenceQueue    = new ReferenceQueue<>();
+		private final List<PhantomReference<Object>> phantomReferences = new LinkedList<>();
+		private long finalizedSoFar = 0L;
+
+		/**
+		 * Track the object in this {@link RetainedDetector}'s {@link ReferenceQueue}
+		 *
+		 * @param object the object to track
+		 * @param <T> the type of the object
+		 * @return the tracked object for further use
+		 */
+		public final <T> T tracked(T object) {
+			phantomReferences.add(new PhantomReference<>(object, referenceQueue));
+			return object;
+		}
+
+		/**
+		 * Returns the number of tracked objects that have been finalized.
+		 * @return the number of tracked object that have been finalized.
+		 */
+		public final synchronized long finalizedCount() {
+			synchronized (this) {
+				while(referenceQueue.poll() != null) {
+					finalizedSoFar++;
+				}
+			}
+			return finalizedSoFar;
+		}
+
+		public final long trackedTotal() {
+			return phantomReferences.size();
+		}
+	}
+
+}
