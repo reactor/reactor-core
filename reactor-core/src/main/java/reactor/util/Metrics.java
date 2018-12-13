35d74fa418cf9eb3bef7245766b50191ccdcbe94
Simon Baslé <sbasle@pivotal.io>
2018-12-13 16:47:58 +0100

    Avoid MeterRegistry field altogether

--- reactor-core/src/main/java/reactor/util/Metrics.java
+++ reactor-core/src/main/java/reactor/util/Metrics.java
@@ -44,8 +44,9 @@ public class Metrics {
 		isMicrometerAvailable = micrometer;
 	}
 
+	//intentionally an Object, so that even with eager loading of fields this won't blow up in NoClassDefFoundError
 	@Nullable
-	private static MeterRegistry defaultRegistry;
+	private static Object defaultRegistry;
 
 	/**
 	 * Check if the current runtime supports metrics / instrumentation, by
@@ -57,20 +58,42 @@ public class Metrics {
 		return isMicrometerAvailable;
 	}
 
+	/**
+	 * Attempt to set up a custom default meter registry for the case where {@link #isInstrumentationAvailable()}.
+	 * <p>
+	 * To avoid referring to micrometer classes, this method takes an arbitrary {@link Object},
+	 * and is thus type-unsafe. The candidate is only accepted if it is a suitable {@code MeterRegistry},
+	 * provided instrumentation is indeed available. Otherwise it is ignored and this method
+	 * returns false.
+	 *
+	 * @param registryCandidate the candidate {@link Object} to set up as meter registry.
+	 * Use {@literal null} to reset.
+	 * @return true if the candidate was accepted, false otherwise.
+	 */
 	public static final boolean setUnsafeRegistry(@Nullable Object registryCandidate) {
 		if (isMicrometerAvailable) {
 			if (registryCandidate == null || registryCandidate instanceof MeterRegistry) {
-				defaultRegistry = (MeterRegistry) registryCandidate;
+				defaultRegistry = registryCandidate;
 				return true;
 			}
 		}
 		return false;
 	}
 
+	/**
+	 * Returns a meter registry IF {@link #isInstrumentationAvailable()} (either the global
+	 * one or a suitable registry that has been set via {@link #setUnsafeRegistry(Object)}),
+	 * or null otherwise. If the value is not null, it can always be casted to a {@code MeterRegistry}.
+	 * <p>
+	 * To avoid referring to micrometer classes, this method returns an {@link Object}, and
+	 * is thus type-unsafe.
+	 *
+	 * @return an {@link Object} that is a suitable default meter registry if relevant, null otherwise.
+	 */
 	@Nullable
 	public static final Object getUnsafeRegistry() {
 		if (isMicrometerAvailable) {
-			return defaultRegistry == null ? globalRegistry : defaultRegistry;
+			return defaultRegistry != null ? defaultRegistry : globalRegistry;
 		}
 		return null;
 	}
