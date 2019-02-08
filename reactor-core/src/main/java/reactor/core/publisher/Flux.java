24e9800c42746b9bb928136d211e227d6f6e7c4e
Oleh Dokuka <shadowgun@i.ua>
2019-02-08 17:42:20 +0200

    fixes docs

--- reactor-core/src/main/java/reactor/core/publisher/Flux.java
+++ reactor-core/src/main/java/reactor/core/publisher/Flux.java
@@ -7966,6 +7966,7 @@ public abstract class Flux<T> implements Publisher<T> {
 	 *      }
 	 *      return flux; //either early complete or error, this forwards the termination in any case
 	 *      //`return flux.onErrorResume(t -> Mono.empty());` instead would suppress an early error
+	 *      //`return Flux.just(1,2,3);` instead would suppress an early error and return 1, 2, 3
 	 *  })
 	 * }
 	 * </pre></blockquote>
@@ -7974,30 +7975,8 @@ public abstract class Flux<T> implements Publisher<T> {
 	 * <p>
 	 * It is advised to return a {@link Publisher} derived from the original {@link Flux}
 	 * in all cases, as not doing so would keep the original {@link Publisher} open and
-	 * hanging with a single request by the terminal signal of the returned
-	 * {@link Publisher}. Note, in such case only the terminal signal of the returned
-	 * {@link Publisher} or general stream cancellation will cancel the original
-	 * {@link Flux}.
-	 * <p>
-	 * Note, returning {@link Publisher} derived from the different source than original
-	 * will suppress all upstream signal.
-	 * <p>
-	 * For example, the following stream will return 3, 4, 5 and complete successfully
-	 * ignoring all signals from the upstream:
-     * <blockquote><pre>
-	 * {@code
-	 *  StepVerifier.create(
-	 *                  Flux.just(1)
-	 *                      .concatWith(Flux.error(new RuntimeException())
-	 *                      .switchOnFirst((s, flux) -> {
-	 *                          Assert.assertEquals(s.getType(), SignalType.ON_NEXT);
-	 *                          return Flux.just(3, 4, 5);
-	 *                      })
-	 *              )
-	 *              .expectNext(3, 4, 5)
-	 *              .verifyComplete();
-	 * }
-	 * </pre></blockquote>
+	 * hanging with a single request until the inner {@link Publisher} terminates or
+	 * the whole {@link Flux} is cancelled.
 	 *
 	 * @param transformer A {@link BiFunction} executed once the first signal is
 	 * available and used to transform the source conditionally. The whole source (including
