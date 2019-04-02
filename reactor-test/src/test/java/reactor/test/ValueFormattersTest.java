823c56e69c40b23bb2c342c72622a98b9096a44b
Simon Baslé <sbasle@pivotal.io>
2019-04-02 15:11:06 +0200

    complete tests

--- reactor-test/src/test/java/reactor/test/ValueFormattersTest.java
+++ reactor-test/src/test/java/reactor/test/ValueFormattersTest.java
@@ -16,10 +16,15 @@
 
 package reactor.test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 
 import org.junit.Test;
+
+import reactor.core.publisher.Signal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -102,5 +107,109 @@ public class ValueFormattersTest {
 				.isEqualTo("OB2");
 	}
 
-	//TODO test signal, iterable, array
+	@Test
+	public void signalDoesntConsiderNonSignal() {
+		Function<Object, String> formatter = ValueFormatters.signalOf(Object.class, o -> o.getClass().getSimpleName());
+		Object target = Obscure.OB1;
+
+		assertThat(formatter.apply(target)).isEqualTo(target.toString());
+	}
+
+	@Test
+	public void signalDoesntConsiderNonMatchingContent() {
+		Function<Object, String> formatter = ValueFormatters.signalOf(Number.class, o -> o.getClass().getSimpleName());
+		Signal<Object> target = Signal.next(Obscure.OB1);
+
+		assertThat(formatter.apply(target)).isEqualTo(target.toString());
+	}
+
+	@Test
+	public void signalDoesntConvertComplete() {
+		Function<Object, String> formatter = ValueFormatters.signalOf(Number.class, o -> o.getClass().getSimpleName());
+		Signal<Number> target = Signal.complete();
+
+		assertThat(formatter.apply(target)).isEqualTo(target.toString());
+	}
+
+	@Test
+	public void signalDoesntConvertError() {
+		Function<Object, String> formatter = ValueFormatters.signalOf(Number.class, o -> o.getClass().getSimpleName());
+		Signal<Number> target = Signal.error(new IllegalStateException("foo"));
+
+		assertThat(formatter.apply(target)).isEqualTo(target.toString());
+	}
+
+	@Test
+	public void signalDoesntConvertErrorEvenIfThrowableClass() {
+		Function<Object, String> formatter = ValueFormatters.signalOf(Throwable.class, o -> o.getClass().getSimpleName());
+		Signal<Number> target = Signal.error(new IllegalStateException("foo"));
+
+		assertThat(formatter.apply(target)).isEqualTo(target.toString());
+	}
+
+	@Test
+	public void signalConvertsOnNextContentMatching() {
+		Function<Object, String> formatter = ValueFormatters.signalOf(Obscure.class, Obscure::getAlternative);
+		Signal<Obscure> target = Signal.next(Obscure.OB1);
+
+		assertThat(formatter.apply(target))
+				.isNotEqualTo(target.toString())
+				.isEqualTo("onNext(foo)");
+	}
+
+	@Test
+	public void iterableDoesntConsiderNonIterable() {
+		Function<Object, String> formatter = ValueFormatters.iterableOf(Object.class, o -> o.getClass().getSimpleName());
+		Object target = Obscure.OB1;
+
+		assertThat(formatter.apply(target)).isEqualTo(target.toString());
+	}
+
+	@Test
+	public void iterableDoesntConsiderNonMatchingContent() {
+		Function<Object, String> formatter = ValueFormatters.iterableOf(Number.class, o -> o.getClass().getSimpleName());
+		List<Object> target = Collections.singletonList(Obscure.OB1);
+
+		assertThat(formatter.apply(target)).isEqualTo(target.toString());
+	}
+
+	@Test
+	public void iterableConvertsContentMatching() {
+		Function<Object, String> formatter = ValueFormatters.iterableOf(Obscure.class, Obscure::getAlternative);
+		List<Object> target = Arrays.asList(1L, 2d, "foo", Obscure.OB2);
+
+		assertThat(formatter.apply(target))
+				.isNotEqualTo(target.toString())
+				.isEqualTo("[1, 2.0, foo, bar]")
+				.isEqualTo(target.toString().replace("OB2", "bar"));
+	}
+
+	@Test
+	public void arrayDoesntConsiderNonArray() {
+		Function<Object, String> formatter = ValueFormatters.arrayOf(Object.class, o -> o.getClass().getSimpleName());
+		Object target = Obscure.OB1;
+
+		assertThat(formatter.apply(target)).isEqualTo(target.toString());
+	}
+
+	@Test
+	public void arrayDisplaysToStringOfNonMatchingContent() {
+		Function<Object, String> formatter = ValueFormatters.arrayOf(Number.class, o -> o.getClass().getSimpleName());
+		String[] target = new String[] { "foo", "bar" };
+
+		assertThat(formatter.apply(target))
+				.isNotEqualTo(target.toString())
+				.isEqualTo("[foo, bar]");
+	}
+
+	@Test
+	public void arrayConvertsContentMatching() {
+		Function<Object, String> formatter = ValueFormatters.arrayOf(Obscure.class, Obscure::getAlternative);
+		Object[] target = new Object[]{1L, 2d, "foo", Obscure.OB2};
+
+		assertThat(formatter.apply(target))
+				.isNotEqualTo(target.toString())
+				.isNotEqualTo(target.toString().replace("OB2", "bar"))
+				.isEqualTo("[1, 2.0, foo, bar]");
+	}
 }
