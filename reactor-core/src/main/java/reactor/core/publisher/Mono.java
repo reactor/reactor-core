5c905bcfcdc5cef0f36de98c84057b87c8f9d99c
Johnny Lim <izeye@naver.com>
2019-07-10 04:33:25 +0900

    Add missing spaces in Javadoc

--- reactor-core/src/main/java/reactor/core/publisher/Mono.java
+++ reactor-core/src/main/java/reactor/core/publisher/Mono.java
@@ -1501,7 +1501,7 @@ public abstract class Mono<T> implements CorePublisher<T> {
 	 * received or a timeout expires. Returns that value, or null if the Mono completes
 	 * empty. In case the Mono errors, the original exception is thrown (wrapped in a
 	 * {@link RuntimeException} if it was a checked exception).
-	 * If the provided timeout expires,a {@link RuntimeException} is thrown.
+	 * If the provided timeout expires, a {@link RuntimeException} is thrown.
 	 *
 	 * <p>
 	 * <img class="marble" src="doc-files/marbles/blockWithTimeout.svg" alt="">
