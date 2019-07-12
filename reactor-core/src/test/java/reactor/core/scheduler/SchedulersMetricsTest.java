a4523ad472f15e1f50d506c41d321d6a7d302150
Sergei Egorov <segorov@pivotal.io>
2019-07-12 11:33:48 +0200

    Record executor service metrics to Micrometer (#1795)
    Since we were ignoring the result of `ExecutorServiceMetrics.monitor`,
    we were not reporting the execution times of tasks submitted to `ExecutorServices`.
    
    These metrics are super helpful to detect thread starvation
    (by analysing executor_seconds_max, for instance).
    
    This change make it instrument every `Runnable` the same way Micrometer does.

--- reactor-core/src/test/java/reactor/core/scheduler/SchedulersMetricsTest.java
+++ reactor-core/src/test/java/reactor/core/scheduler/SchedulersMetricsTest.java
@@ -1,23 +1,34 @@
 package reactor.core.scheduler;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.Phaser;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+import io.micrometer.core.instrument.FunctionCounter;
 import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Timer;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import reactor.core.Disposable;
+import reactor.core.Disposables;
 
 import reactor.core.Disposable;
 import reactor.core.Disposables;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+import static org.awaitility.Awaitility.await;
+import static reactor.core.scheduler.SchedulerMetricDecorator.TAG_SCHEDULER_ID;
 
 public class SchedulersMetricsTest {
 
 	final SimpleMeterRegistry simpleMeterRegistry = new SimpleMeterRegistry();
 	final Disposable.Composite toCleanUp = Disposables.composite();
+
+	final Disposable.Composite disposables = Disposables.composite();
 
 	@Before
 	public void setUp() {
@@ -27,6 +38,7 @@ public class SchedulersMetricsTest {
 
 	@After
 	public void tearDown() {
+		disposables.dispose();
 		Schedulers.disableMetrics();
 		Metrics.globalRegistry.forEachMeter(Metrics.globalRegistry::remove);
 		Metrics.removeRegistry(simpleMeterRegistry);
@@ -70,7 +82,7 @@ public class SchedulersMetricsTest {
 
 		assertThat(simpleMeterRegistry.getMeters()
 		                              .stream()
-		                              .map(m -> m.getId().getTag(SchedulerMetricDecorator.TAG_SCHEDULER_ID))
+		                              .map(m -> m.getId().getTag(TAG_SCHEDULER_ID))
 		                              .distinct())
 				.containsOnly(
 						"parallel(4,\"A\")",
@@ -168,5 +180,75 @@ public class SchedulersMetricsTest {
 		                              .map(m -> m.getId().getName())
 		                              .distinct())
 				.containsExactly("foo");
+	}
+
+	@Test
+    public void shouldReportExecutorMetrics() {
+		Scheduler scheduler = Schedulers.newParallel("A", 1);
+		disposables.add(scheduler);
+
+		final int taskCount = 3;
+
+		for (int i = 0; i < taskCount; i++) {
+			scheduler.schedule(() -> {
+			});
+		}
+
+		FunctionCounter counter = simpleMeterRegistry
+				.find("executor.completed")
+				.tag(TAG_SCHEDULER_ID, scheduler.toString())
+				.functionCounter();
+
+		// Use Awaitility because "count" is reported "eventually"
+		await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
+			assertThat(counter)
+					.isNotNull()
+					.satisfies(it -> {
+						assertThat(it.count())
+								.as("count")
+								.isEqualTo(taskCount);
+					});
+		});
+    }
+
+	@Test(timeout = 10_000)
+	public void shouldReportExecutionTimes() {
+		Scheduler scheduler = Schedulers.newParallel("A", 1);
+		disposables.add(scheduler);
+
+		final int taskCount = 3;
+
+		Phaser phaser = new Phaser(1);
+		for (int i = 1; i <= taskCount; i++) {
+			phaser.register();
+			int delay = i * 20;
+			scheduler.schedule(() -> {
+				try {
+					Thread.sleep(delay);
+					phaser.arriveAndDeregister();
+				}
+				catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			});
+		}
+		phaser.arriveAndAwaitAdvance();
+
+		Timer timer = simpleMeterRegistry
+				.find("executor")
+				.tag(TAG_SCHEDULER_ID, scheduler.toString())
+				.timer();
+
+		// Use Awaitility because "count" is reported "eventually"
+		await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
+			assertThat(timer)
+					.isNotNull()
+					.satisfies(it -> {
+						assertThat(it.count()).as("count").isEqualTo(taskCount);
+						assertThat(it.max(TimeUnit.MILLISECONDS))
+								.as("min")
+								.isEqualTo(60, offset(10.0d));
+					});
+		});
 	}
 }
