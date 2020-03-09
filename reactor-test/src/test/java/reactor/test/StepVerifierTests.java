61b5c02a9242b7d8214c1b0bf9df1d8ad1b37a75
Simon Baslé <sbasle@pivotal.io>
2020-03-09 15:05:38 +0100

    fix #2060 Introduce and use VirtualTimeScheduler#getOrSet(boolean)
    A change in #2017 introduced a regression that prevented externally
    initialized VirtualTimeSchedulers to be used in StepVerifier.
    This commit allows the StepVerifier to once again discover an already
    set VTS instance and use it, taking the `defer` parameter into account.

--- reactor-test/src/test/java/reactor/test/StepVerifierTests.java
+++ reactor-test/src/test/java/reactor/test/StepVerifierTests.java
@@ -29,18 +29,20 @@ import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.locks.LockSupport;
-import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Ignore;
-import org.junit.Rule;
 import org.junit.Test;
+
 import reactor.core.Fuseable;
 import reactor.core.publisher.DirectProcessor;
-import reactor.core.publisher.MonoProcessor;
+import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoProcessor;
 import reactor.core.publisher.UnicastProcessor;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
@@ -2342,5 +2344,35 @@ public class StepVerifierTests {
 				.verifyComplete();
 
 		assertThat(requests).containsExactly(1L, 1L, 1L);
+	}
+
+	//see https://github.com/reactor/reactor-core/issues/2060
+	@Test
+	public void externalGetOrSetTakenIntoAccount() {
+		Scheduler.Worker subscriptionWorker = VirtualTimeScheduler.getOrSet().createWorker();
+		List<String> source = Stream.of("first", "second", "third").collect(Collectors.toList());
+
+
+		StepVerifier.withVirtualTime(() -> {
+			EmitterProcessor<String> fluxEmitter = EmitterProcessor.create();
+
+			subscriptionWorker.schedulePeriodically(() -> {
+				if (source.size() > 0) {
+					fluxEmitter.onNext(source.remove(0));
+				}
+				else {
+					fluxEmitter.onComplete();
+				}
+			}, 0, 10, TimeUnit.MILLISECONDS);
+			return fluxEmitter;
+		})
+		            .expectNext("first")
+		            .expectNoEvent(Duration.ofMillis(10))
+		            .expectNext("second")
+		            .expectNoEvent(Duration.ofMillis(10))
+		            .expectNext("third")
+		            .expectNoEvent(Duration.ofMillis(10))
+		            .expectComplete()
+		            .verify(Duration.ofSeconds(2));
 	}
 }
