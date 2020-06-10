8de5bf3e5d9aea38a0476ebf5ce56519342715f2
Eric Bottard <313494+ericbottard@users.noreply.github.com>
2020-06-10 11:20:01 +0200

    Fix #2181 Handle nulls from collectors in Flux.collect()
    Emit an NPE early instead of signaling onNext(null).

--- reactor-core/src/main/java/reactor/core/publisher/MonoStreamCollector.java
+++ reactor-core/src/main/java/reactor/core/publisher/MonoStreamCollector.java
@@ -164,6 +164,10 @@ final class MonoStreamCollector<T, A, R> extends MonoFromFluxOperator<T, R>
 				return;
 			}
 
+			if (r == null) {
+				actual.onError(Operators.onOperatorError(new NullPointerException("Collector returned null"), actual.currentContext()));
+				return;
+			}
 			complete(r);
 		}
 
