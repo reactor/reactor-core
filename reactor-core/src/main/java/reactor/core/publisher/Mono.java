5bd1eb2b405ea45acf5c2329e5598e3841a6127f
Sean <sts423@gmail.com>
2020-07-23 11:43:21 -0400

    [doc] Fix Mono#subscribeOn doc typo (#2286)
    The method returns a Mono, not a Flux.

--- reactor-core/src/main/java/reactor/core/publisher/Mono.java
+++ reactor-core/src/main/java/reactor/core/publisher/Mono.java
@@ -3863,7 +3863,7 @@ public abstract class Mono<T> implements CorePublisher<T> {
 	 *
 	 * @param scheduler a {@link Scheduler} providing the {@link Worker} where to subscribe
 	 *
-	 * @return a {@link Flux} requesting asynchronously
+	 * @return a {@link Mono} requesting asynchronously
 	 * @see #publishOn(Scheduler)
 	 */
 	public final Mono<T> subscribeOn(Scheduler scheduler) {
