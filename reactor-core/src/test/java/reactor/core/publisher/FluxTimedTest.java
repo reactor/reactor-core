7d94f375967f0d4db182345640e8ae2d756617ec
Simon Baslé <sbasle@vmware.com>
2020-11-04 14:02:45 +0100

    make initial tests use virtual time

--- reactor-core/src/test/java/reactor/core/publisher/FluxTimedTest.java
+++ reactor-core/src/test/java/reactor/core/publisher/FluxTimedTest.java
@@ -22,54 +22,75 @@ import java.util.function.Consumer;
 import org.junit.jupiter.api.Test;
 
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 import reactor.util.function.Tuple2;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static reactor.core.publisher.Sinks.EmitFailureHandler.FAIL_FAST;
 
 class FluxTimedTest {
 
+	private static Runnable emit(String value, Duration delay, Sinks.Many<String> sink, VirtualTimeScheduler vts) {
+		return () -> {
+			vts.advanceTimeBy(delay);
+			sink.emitNext(value, FAIL_FAST);
+		};
+	}
+
 	@Test
 	void compareTimedElapsedNanosToElapsedT1() {
-		Consumer<? super Timed<Tuple2<Long, Integer>>> timedSimilarToElapsed = v -> {
+		//we'll manipulate the vts and emit values manually in order to show what happens with half milliseconds
+		VirtualTimeScheduler vts = VirtualTimeScheduler.create();
+		Sinks.Many<String> sink = Sinks.unsafe().many().multicast().directBestEffort();
+
+		Consumer<? super Timed<Tuple2<Long, String>>> timedSimilarToElapsed = v -> {
 			assertThat(v.elapsed().toMillis())
 					.as(v.toString())
 					//Duration.toMillis() drops the nanosecond part entirely
+					.isEqualTo(123L)
 					.isBetween(v.get().getT1() - 1L, v.get().getT1());
 		};
 
-		Flux.range(1, 5)
-		    .delayElements(Duration.ofMillis(123))
-		    .elapsed()
-		    .timed()
-		    .as(StepVerifier::create)
-		    .assertNext(timedSimilarToElapsed)
-		    .assertNext(timedSimilarToElapsed)
-		    .assertNext(timedSimilarToElapsed)
-		    .assertNext(timedSimilarToElapsed)
-		    .assertNext(timedSimilarToElapsed)
-		    .verifyComplete();
+		StepVerifier.withVirtualTime(() -> sink.asFlux().elapsed().timed(),
+				() -> vts, Long.MAX_VALUE)
+		            .then(emit("A", Duration.ofNanos(123 * 1_000_000), sink, vts))
+		            .assertNext(timedSimilarToElapsed)
+		            .then(emit("B", Duration.ofNanos(1234 * 100_000), sink, vts))
+		            .assertNext(timedSimilarToElapsed)
+		            .then(emit("C", Duration.ofNanos(1235 * 100_000), sink, vts))
+		            .assertNext(timedSimilarToElapsed)
+		            .then(emit("D", Duration.ofNanos(12355 * 10_000), sink, vts)) //this is the one that gets truncated down
+		            .assertNext(timedSimilarToElapsed)
+		            .then(() -> sink.tryEmitComplete().orThrow())
+		            .verifyComplete();
 	}
 
 	@Test
 	void compareTimedTimetampNanosToTimestampT1() {
-		Consumer<? super Timed<Tuple2<Long, Integer>>> timedSimilarToTimestamp = v -> {
+		//we'll manipulate the vts and emit values manually in order to show what happens with half milliseconds
+		VirtualTimeScheduler vts = VirtualTimeScheduler.create();
+		Sinks.Many<String> sink = Sinks.unsafe().many().multicast().directBestEffort();
+
+		Consumer<? super Timed<Tuple2<Long, String>>> timedSimilarToTimestamp = v -> {
 			assertThat(v.timestamp().toEpochMilli())
 					.as(v.toString())
+					.isLessThan(1000L)
 					//Instant.toEpochMilli() drops the nanosecond part entirely
 					.isBetween(v.get().getT1() -1L, v.get().getT1());
 		};
 
-		Flux.range(1, 5)
-		    .delayElements(Duration.ofMillis(123))
-		    .timestamp()
-		    .timed()
-		    .as(StepVerifier::create)
-		    .assertNext(timedSimilarToTimestamp)
-		    .assertNext(timedSimilarToTimestamp)
-		    .assertNext(timedSimilarToTimestamp)
-		    .assertNext(timedSimilarToTimestamp)
-		    .assertNext(timedSimilarToTimestamp)
-		    .verifyComplete();
+		StepVerifier.withVirtualTime(() -> sink.asFlux().timestamp().timed(),
+				() -> vts, Long.MAX_VALUE)
+		            .then(emit("A", Duration.ofNanos(123 * 1_000_000), sink, vts))
+		            .assertNext(timedSimilarToTimestamp)
+		            .then(emit("B", Duration.ofNanos(1234 * 100_000), sink, vts))
+		            .assertNext(timedSimilarToTimestamp)
+		            .then(emit("C", Duration.ofNanos(1235 * 100_000), sink, vts))
+		            .assertNext(timedSimilarToTimestamp)
+		            .then(emit("D", Duration.ofNanos(12355 * 10_000), sink, vts)) //this is the one that gets truncated down
+		            .assertNext(timedSimilarToTimestamp)
+		            .then(() -> sink.tryEmitComplete().orThrow())
+		            .verifyComplete();
 	}
 
 }
