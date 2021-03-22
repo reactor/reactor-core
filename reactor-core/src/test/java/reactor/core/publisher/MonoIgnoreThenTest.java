64649ff5171bac1cf385f63b8d5f9f6749eda597
Simon Baslé <sbasle@vmware.com>
2021-03-22 15:31:01 +0100

    Polish #2660/#2659: formatting+license, inline scenario() method

--- reactor-core/src/test/java/reactor/core/publisher/MonoIgnoreThenTest.java
+++ reactor-core/src/test/java/reactor/core/publisher/MonoIgnoreThenTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
+ * Copyright (c) 2011-Present VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       https://www.apache.org/licenses/LICENSE-2.0
+ *        https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,7 +19,6 @@ import java.time.Duration;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.reactivestreams.Publisher;
 
 import reactor.core.Disposable;
 import reactor.core.scheduler.Scheduler;
@@ -34,23 +33,18 @@ class MonoIgnoreThenTest {
 	@Nested
 	class ThenIgnoreVariant {
 
-		Publisher<Void> scenario(){
-			return Mono.just(1)
-			           .thenEmpty(Mono.delay(Duration.ofSeconds(123)).then());
-		}
-
 		@Test
 		void justThenIgnore() {
 			StepVerifier.create(Mono.just(1)
-			                        .then())
-			            .verifyComplete();
+									.then())
+						.verifyComplete();
 		}
 
 		@Test
 		void justThenEmptyThenIgnoreWithTime() {
-			StepVerifier.withVirtualTime(this::scenario)
-			            .thenAwait(Duration.ofSeconds(123))
-			            .verifyComplete();
+			StepVerifier.withVirtualTime(() -> Mono.just(1).thenEmpty(Mono.delay(Duration.ofSeconds(123)).then()))
+						.thenAwait(Duration.ofSeconds(123))
+						.verifyComplete();
 		}
 
 		@Test
@@ -76,57 +70,56 @@ class MonoIgnoreThenTest {
 				Mono.fromSupplier(() -> Thread.currentThread().getName());
 		for (int i = 0; i < 100000; i++) {
 			StepVerifier.create(getCurrentThreadName.publishOn(scheduler)
-			                                        .then(getCurrentThreadName)
-			                                        .then(getCurrentThreadName)
-			                                        .then(getCurrentThreadName))
-			            .assertNext(threadName -> {
-				            assertThat(threadName).startsWith("non-test-thread");
-			            })
-			            .verifyComplete();
+													.then(getCurrentThreadName)
+													.then(getCurrentThreadName)
+													.then(getCurrentThreadName))
+						.assertNext(threadName -> {
+							assertThat(threadName).startsWith("non-test-thread");
+						})
+						.verifyComplete();
 		}
 	}
 
 	@Test
 	void justThenEmpty() {
 		StepVerifier.create(Mono.just(1)
-		                        .thenEmpty(Flux.empty()))
-		            .verifyComplete();
+								.thenEmpty(Flux.empty()))
+					.verifyComplete();
 	}
 
 	@Test
 	void justThenThen() {
 		StepVerifier.create(Mono.just(0)
-		                        .then(Mono.just(1))
-		                        .then(Mono.just(2)))
-		            .expectNext(2)
-		            .verifyComplete();
+								.then(Mono.just(1))
+								.then(Mono.just(2)))
+					.expectNext(2)
+					.verifyComplete();
 	}
 
-
 	@Test
-    void justThenReturn() {
-	    StepVerifier.create(Mono.just(0).thenReturn(2))
-                    .expectNext(2)
-                    .verifyComplete();
-    }
+	void justThenReturn() {
+		StepVerifier.create(Mono.just(0).thenReturn(2))
+					.expectNext(2)
+					.verifyComplete();
+	}
 
 	//see https://github.com/reactor/reactor-core/issues/661
 	@Test
 	void fluxThenMonoAndShift() {
 		StepVerifier.create(Flux.just("Good Morning", "Hello")
-		                        .then(Mono.just("Good Afternoon"))
-		                        .then(Mono.just("Bye")))
-		            .expectNext("Bye")
-		            .verifyComplete();
+								.then(Mono.just("Good Afternoon"))
+								.then(Mono.just("Bye")))
+					.expectNext("Bye")
+					.verifyComplete();
 	}
 
 	//see https://github.com/reactor/reactor-core/issues/661
 	@Test
 	void monoThenMonoAndShift() {
 		StepVerifier.create(Mono.just("Good Morning")
-		                        .then(Mono.just("Good Afternoon"))
-		                        .then(Mono.just("Bye")))
-		            .expectNext("Bye")
-		            .verifyComplete();
+								.then(Mono.just("Good Afternoon"))
+								.then(Mono.just("Bye")))
+					.expectNext("Bye")
+					.verifyComplete();
 	}
 }
