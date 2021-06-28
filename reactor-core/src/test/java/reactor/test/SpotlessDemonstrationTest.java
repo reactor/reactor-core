765bf8f8ad810e2487affa543f6b80beac487317
Simon Baslé <sbasle@vmware.com>
2021-06-28 11:30:21 +0200

    Same demonstration with spotlessApply invoked

--- reactor-core/src/test/java/reactor/test/SpotlessDemonstrationTest.java
+++ reactor-core/src/test/java/reactor/test/SpotlessDemonstrationTest.java
@@ -1,13 +1,25 @@
+/*
+ * Copyright (c) 2011-2021 VMware Inc. or its affiliates, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package reactor.test;
 
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.Test;
 
 import reactor.core.publisher.Mono;
-import reactor.test.subscriber.AssertSubscriber;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Simon Baslé
@@ -26,7 +38,7 @@ class SpotlessDemonstrationTest {
 	@Test
 	void withTooMuchAlignmentSpaces() {
 		Mono.defer(() -> Mono.just(1)
-		                     .doOnTerminate(() -> { } ))
+							 .doOnTerminate(() -> { } ))
 			.subscribe();
 	}
 
