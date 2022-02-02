504feb791f808670da8fb2e8504d986a7e469aa5
Simon Baslé <sbasle@vmware.com>
2022-02-02 10:19:35 +0100

    Add BlockHound exception for BoundedElasticScheduler ensureQueueCapacity

--- reactor-core/src/blockHoundTest/java/reactor/core/scheduler/BoundedElasticSchedulerBlockhoundTest.java
+++ reactor-core/src/blockHoundTest/java/reactor/core/scheduler/BoundedElasticSchedulerBlockhoundTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2021 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2016-2022 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,28 +16,122 @@
 
 package reactor.core.scheduler;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicReference;
 
-
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
 import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
 import reactor.core.Disposable;
+import reactor.core.Disposables;
+import reactor.core.Exceptions;
+import reactor.test.util.RaceTestUtils;
+import reactor.util.Logger;
+import reactor.util.Loggers;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BoundedElasticSchedulerBlockhoundTest {
 
+	private static final Logger     LOGGER  = Loggers.getLogger(BoundedElasticSchedulerBlockhoundTest.class);
+
+	private Disposable.Composite toDispose;
+
 	@BeforeAll
-	public static void setup() {
+	static void setup() {
 		BlockHound.install();
+	}
+
+	@BeforeEach
+	void setupComposite() {
+		toDispose = Disposables.composite();
+	}
+
+	@AfterEach
+	void dispose() {
+		toDispose.dispose();
+	}
+
+	<T extends Disposable> T autoDispose(T disposable) {
+		toDispose.add(disposable);
+		return disposable;
+	}
+
+	@Test
+	void smokeTestBlockhound() throws InterruptedException, TimeoutException {
+		try {
+			FutureTask<?> task = new FutureTask<>(() -> {
+				Thread.sleep(0);
+				return "";
+			});
+			Schedulers.parallel().schedule(task);
+
+			task.get(10, TimeUnit.SECONDS);
+			Assertions.fail("should fail");
+		} catch (ExecutionException e) {
+			assertThat(e).hasCauseInstanceOf(BlockingOperationError.class);
+		}
 	}
 
 	//see https://github.com/reactor/reactor-core/issues/2143
 	@Test
-	public void shouldNotReportBlockingCallWithZoneIdUsage() throws Throwable {
+	void shouldNotReportBlockingCallWithZoneIdUsage() throws Throwable {
 		FutureTask<Disposable> testTask = new FutureTask<>(() -> new BoundedElasticScheduler(1, 1, Thread::new, 1));
 		Schedulers.single().schedule(testTask);
 		//automatically re-throw in case of blocking call, in which case the scheduler hasn't been created so there is no leak.
 		testTask.get().dispose();
 	}
 
+	@RepeatedTest(3) //we got false positives from time to time. with repeat(3), only 1 block out of 500 was a false positive (with a total of 75 false positives out of 1500 runs)
+	void shouldNotReportEnsureQueueCapacity() {
+		BoundedElasticScheduler scheduler = autoDispose(new BoundedElasticScheduler(1, 100, Thread::new, 1));
+		scheduler.start();
+		ExecutorServiceWorker worker = (ExecutorServiceWorker) autoDispose(scheduler.createWorker());
+		BoundedElasticScheduler.BoundedScheduledExecutorService executor =
+			(BoundedElasticScheduler.BoundedScheduledExecutorService) worker.exec;
+
+		AtomicReference<Throwable> error = new AtomicReference<>();
+		Runnable runnable = () -> {
+			try {
+				executor.ensureQueueCapacity(1);
+			}
+			catch (final Throwable t) {
+				error.updateAndGet(current -> {
+					if (current == null) return t;
+					List<Throwable> multiple = new ArrayList<>(Exceptions.unwrapMultiple(current));
+					multiple.add(t);
+					return Exceptions.multiple(multiple);
+				});
+			}
+		};
+
+		Scheduler sch = autoDispose(Schedulers.newParallel("test", 10));
+
+		RaceTestUtils.race(sch,
+			runnable,
+			runnable,
+			runnable,
+			runnable,
+			runnable,
+			runnable,
+			runnable,
+			runnable,
+			runnable,
+			runnable);
+
+		//assertions don't really show the stack trace unless we modify a global config
+		//so here we simply throw the composite if there is one
+		if (error.get() != null) throw Exceptions.propagate(error.get());
+	}
 }
