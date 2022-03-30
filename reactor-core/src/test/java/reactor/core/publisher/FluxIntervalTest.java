1be4cd08386939460b027e870bfe377702c0302e
Simon Baslé <sbasle@vmware.com>
2022-03-30 10:53:03 +0200

    test interval and onBackpressureBufferTimeout

--- reactor-core/src/test/java/reactor/core/publisher/FluxIntervalTest.java
+++ reactor-core/src/test/java/reactor/core/publisher/FluxIntervalTest.java
@@ -19,16 +19,22 @@ package reactor.core.publisher;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
 import reactor.core.CoreSubscriber;
 import reactor.core.Exceptions;
 import reactor.core.Scannable;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+import reactor.test.TestGenerationUtils;
 import reactor.test.subscriber.AssertSubscriber;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -211,4 +217,14 @@ public class FluxIntervalTest {
 		            .thenCancel()
 		            .verify();
     }
+
+	@TestFactory
+	Stream<DynamicTest> scheduledWithContextInScope() {
+		return TestGenerationUtils.generateScheduledWithContextInScopeTests("interval",
+			Flux.interval(Duration.ofMillis(50)).take(3, false),
+			Function.identity(),
+			helper -> helper.mappingTest()
+				.expectNext("0customized", "1customized", "2customized")
+				.verifyComplete());
+	}
 }
