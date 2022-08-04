8e28a86cf3abe1f802febd3c1ca1791dd0453b8a
Simon Baslé <sbasle@vmware.com>
2022-08-04 10:15:01 +0200

    fix Micrometer test now that we've added a tag

--- reactor-core-micrometer/src/test/java/reactor/core/observability/micrometer/MicrometerTest.java
+++ reactor-core-micrometer/src/test/java/reactor/core/observability/micrometer/MicrometerTest.java
@@ -69,7 +69,7 @@ class MicrometerTest {
 			assertThat(id.getName()).as("prefix used")
 				.isEqualTo("testSchedulerMetrics.scheduler.tasks.submitted");
 			assertThat(id.getTags()).as("tags")
-				.containsExactlyElementsOf(tags);
+				.containsExactlyElementsOf(tags.and(TimedScheduler.TAG_SUBMISSION, TimedScheduler.SUBMISSION_DIRECT));
 		});
 	}
 }
