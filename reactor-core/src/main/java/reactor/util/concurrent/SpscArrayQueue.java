9a0f4ffa2f6cebb2f0e4ef0a58e539bb3a20d70f
Aleksander Lantsov <lantsov.aleksander@gmail.com>
2022-08-24 20:36:18 +0300

    Replace 'long' padding by 'byte' padding to avoid re-ordering on modern JDK

--- reactor-core/src/main/java/reactor/util/concurrent/SpscArrayQueue.java
+++ reactor-core/src/main/java/reactor/util/concurrent/SpscArrayQueue.java
@@ -178,9 +178,23 @@ class SpscArrayQueueCold<T> extends AtomicReferenceArray<T> {
 class SpscArrayQueueP1<T> extends SpscArrayQueueCold<T> {
 	/** */
 	private static final long serialVersionUID = -4461305682174876914L;
-	
-	long p00, p01, p02, p03, p04, p05, p06, p07;
-	long p08, p09, p0A, p0B, p0C, p0D, p0E;
+
+	byte b000,b001,b002,b003,b004,b005,b006,b007;//  8b
+	byte b010,b011,b012,b013,b014,b015,b016,b017;// 16b
+	byte b020,b021,b022,b023,b024,b025,b026,b027;// 24b
+	byte b030,b031,b032,b033,b034,b035,b036,b037;// 32b
+	byte b040,b041,b042,b043,b044,b045,b046,b047;// 40b
+	byte b050,b051,b052,b053,b054,b055,b056,b057;// 48b
+	byte b060,b061,b062,b063,b064,b065,b066,b067;// 56b
+	byte b070,b071,b072,b073,b074,b075,b076,b077;// 64b
+	byte b100,b101,b102,b103,b104,b105,b106,b107;// 72b
+	byte b110,b111,b112,b113,b114,b115,b116,b117;// 80b
+	byte b120,b121,b122,b123,b124,b125,b126,b127;// 88b
+	byte b130,b131,b132,b133,b134,b135,b136,b137;// 96b
+	byte b140,b141,b142,b143,b144,b145,b146,b147;//104b
+	byte b150,b151,b152,b153,b154,b155,b156,b157;//112b
+	byte b160,b161,b162,b163,b164,b165,b166,b167;//120b
+	byte b170,b171,b172,b173,b174,b175,b176,b177;//128b
 
 	SpscArrayQueueP1(int length) {
 		super(length);
@@ -206,9 +220,23 @@ class SpscArrayQueueProducer<T> extends SpscArrayQueueP1<T> {
 class SpscArrayQueueP2<T> extends SpscArrayQueueProducer<T> {
 	/** */
 	private static final long serialVersionUID = -5400235061461013116L;
-	
-	long p00, p01, p02, p03, p04, p05, p06, p07;
-	long p08, p09, p0A, p0B, p0C, p0D, p0E;
+
+	byte b000,b001,b002,b003,b004,b005,b006,b007;//  8b
+	byte b010,b011,b012,b013,b014,b015,b016,b017;// 16b
+	byte b020,b021,b022,b023,b024,b025,b026,b027;// 24b
+	byte b030,b031,b032,b033,b034,b035,b036,b037;// 32b
+	byte b040,b041,b042,b043,b044,b045,b046,b047;// 40b
+	byte b050,b051,b052,b053,b054,b055,b056,b057;// 48b
+	byte b060,b061,b062,b063,b064,b065,b066,b067;// 56b
+	byte b070,b071,b072,b073,b074,b075,b076,b077;// 64b
+	byte b100,b101,b102,b103,b104,b105,b106,b107;// 72b
+	byte b110,b111,b112,b113,b114,b115,b116,b117;// 80b
+	byte b120,b121,b122,b123,b124,b125,b126,b127;// 88b
+	byte b130,b131,b132,b133,b134,b135,b136,b137;// 96b
+	byte b140,b141,b142,b143,b144,b145,b146,b147;//104b
+	byte b150,b151,b152,b153,b154,b155,b156,b157;//112b
+	byte b160,b161,b162,b163,b164,b165,b166,b167;//120b
+	byte b170,b171,b172,b173,b174,b175,b176,b177;//128b
 
 	SpscArrayQueueP2(int length) {
 		super(length);
@@ -234,9 +262,23 @@ class SpscArrayQueueConsumer<T> extends SpscArrayQueueP2<T> {
 class SpscArrayQueueP3<T> extends SpscArrayQueueConsumer<T> {
 	/** */
 	private static final long serialVersionUID = -2684922090021364171L;
-	
-	long p00, p01, p02, p03, p04, p05, p06, p07;
-	long p08, p09, p0A, p0B, p0C, p0D, p0E;
+
+	byte b000,b001,b002,b003,b004,b005,b006,b007;//  8b
+	byte b010,b011,b012,b013,b014,b015,b016,b017;// 16b
+	byte b020,b021,b022,b023,b024,b025,b026,b027;// 24b
+	byte b030,b031,b032,b033,b034,b035,b036,b037;// 32b
+	byte b040,b041,b042,b043,b044,b045,b046,b047;// 40b
+	byte b050,b051,b052,b053,b054,b055,b056,b057;// 48b
+	byte b060,b061,b062,b063,b064,b065,b066,b067;// 56b
+	byte b070,b071,b072,b073,b074,b075,b076,b077;// 64b
+	byte b100,b101,b102,b103,b104,b105,b106,b107;// 72b
+	byte b110,b111,b112,b113,b114,b115,b116,b117;// 80b
+	byte b120,b121,b122,b123,b124,b125,b126,b127;// 88b
+	byte b130,b131,b132,b133,b134,b135,b136,b137;// 96b
+	byte b140,b141,b142,b143,b144,b145,b146,b147;//104b
+	byte b150,b151,b152,b153,b154,b155,b156,b157;//112b
+	byte b160,b161,b162,b163,b164,b165,b166,b167;//120b
+	byte b170,b171,b172,b173,b174,b175,b176,b177;//128b
 
 	SpscArrayQueueP3(int length) {
 		super(length);
