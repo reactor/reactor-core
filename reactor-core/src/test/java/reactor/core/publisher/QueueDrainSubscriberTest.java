3d8ef2127b4577cb8b44508877e6ae526b770523
Aleksander Lantsov <lantsov.aleksander@gmail.com>
2022-08-28 13:00:04 +0300

    Extend padding for QueueDrainSubscriber + update test

--- reactor-core/src/test/java/reactor/core/publisher/QueueDrainSubscriberTest.java
+++ reactor-core/src/test/java/reactor/core/publisher/QueueDrainSubscriberTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2021 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2018-2022 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,7 +66,6 @@ public class QueueDrainSubscriberTest {
 				"error"
 		);
 
-		final long minPaddingSize = 120;
-		assertThat(paddingSizes).filteredOn(padSize -> padSize >= minPaddingSize).hasSize(3);
+		assertThat(paddingSizes).containsExactly(120L, 128L, 128L);
 	}
 }
