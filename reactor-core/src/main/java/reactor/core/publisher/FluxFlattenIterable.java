45a3e4bab0c3b894b759895dbc5eccc953f06058
Oleh Dokuka <odokuka@vmware.com>
2023-01-11 12:01:31 +0200

    fixes styles
    Signed-off-by: Oleh Dokuka <odokuka@vmware.com>

--- reactor-core/src/main/java/reactor/core/publisher/FluxFlattenIterable.java
+++ reactor-core/src/main/java/reactor/core/publisher/FluxFlattenIterable.java
@@ -588,7 +588,6 @@ final class FluxFlattenIterable<T, R> extends InternalFluxOperator<T, R> impleme
 							sp = iterable.spliterator();
 							itFinite = FluxIterable.checkFinite(sp);
 
-
 							isEmpty = itFinite ? sp.estimateSize() == 0 : !hasNext(sp);
 						}
 						catch (Throwable exc) {
