2ce16400ef1d82bfd08006c4431a5816c466e6fe
Oleh Dokuka <5380167+OlegDokuka@users.noreply.github.com>
2023-02-14 10:04:10 +0200

    ensures exception is caught if inner fused for `FlatMap` #3353

--- reactor-core/src/main/java/reactor/core/publisher/FluxFlatMap.java
+++ reactor-core/src/main/java/reactor/core/publisher/FluxFlatMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2021 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2016-2023 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -416,6 +416,7 @@ final class FluxFlatMap<T, R> extends InternalFluxOperator<T, R> {
 						onError(Operators.onOperatorError(s, e_, t, ctx));
 					}
 					Operators.onDiscard(t, ctx);
+					tryEmitScalar(null);
 					return;
 				}
 				tryEmitScalar(v);
