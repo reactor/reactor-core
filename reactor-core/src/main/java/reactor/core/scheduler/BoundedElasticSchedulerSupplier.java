301ad97afcc9fc5502b79e5cd0d6ce1a0f8d2506
OlegDokuka <odokuka@vmware.com>
2023-07-05 14:59:11 +0300

    fixes wording
    Signed-off-by: OlegDokuka <odokuka@vmware.com>

--- reactor-core/src/main/java/reactor/core/scheduler/BoundedElasticSchedulerSupplier.java
+++ reactor-core/src/main/java/reactor/core/scheduler/BoundedElasticSchedulerSupplier.java
@@ -35,7 +35,7 @@ class BoundedElasticSchedulerSupplier implements Supplier<Scheduler> {
 	public Scheduler get() {
 		if (DEFAULT_BOUNDED_ELASTIC_ON_VIRTUAL_THREADS) {
 			logger.warn(
-					"Virtual Threads support is not available on the given JVM. Fallbacks to default BoundedElastic setup");
+					"Virtual Threads support is not available on the given JVM. Falling back to default BoundedElastic setup");
 		}
 
 		return newBoundedElastic(DEFAULT_BOUNDED_ELASTIC_SIZE,
