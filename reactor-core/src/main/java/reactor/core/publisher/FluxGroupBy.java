d8f41f2793aebeabede614cef622e8961f0106f3
Oleh Dokuka <5380167+OlegDokuka@users.noreply.github.com>
2023-08-04 15:23:55 +0300

    ensures `GroupedFlux` delivers subscription for the second subscriber  (#3555)
    Signed-off-by: OlegDokuka <odokuka@vmware.com>

--- reactor-core/src/main/java/reactor/core/publisher/FluxGroupBy.java
+++ reactor-core/src/main/java/reactor/core/publisher/FluxGroupBy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2021 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2016-2023 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -718,7 +718,7 @@ final class FluxGroupBy<T, K, V> extends InternalFluxOperator<T, GroupedFlux<K, 
 				drain();
 			}
 			else {
-				actual.onError(new IllegalStateException(
+				Operators.error(actual, new IllegalStateException(
 						"GroupedFlux allows only one Subscriber"));
 			}
 		}
