e083d9187b79e5d8b784a7a70836622125c4c4a5
Violeta Georgieva <violeta.georgieva@broadcom.com>
2024-04-19 12:50:31 +0300

    Fix copyright header

--- reactor-core/src/main/java/reactor/util/annotation/Incubating.java
+++ reactor-core/src/main/java/reactor/util/annotation/Incubating.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 the original author or authors.
+ * Copyright (c) 2024 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
