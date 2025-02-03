46773730a2e6ba02b93822a423023ceca142fcf3
Alex Kim <111270670+Aleexender@users.noreply.github.com>
2025-02-03 19:20:50 +0900

    Remove redundant return in Mono#subscribe (#3966)
    The return statement at the end of the `subscribe(Subscriber)` method
    body was superflous.
    
    Signed-off-by: Aleexender <alexkjh2011@gmail.com>

--- reactor-core/src/main/java/reactor/core/publisher/Mono.java
+++ reactor-core/src/main/java/reactor/core/publisher/Mono.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2024 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2016-2025 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -4577,7 +4577,6 @@ public abstract class Mono<T> implements CorePublisher<T> {
 		}
 		catch (Throwable e) {
 			Operators.reportThrowInSubscribe(subscriber, e);
-			return;
 		}
 	}
 
