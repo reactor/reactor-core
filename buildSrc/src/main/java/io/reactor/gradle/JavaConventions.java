1a5434243ac91b13202408092bd5ba5b8678a47b
Dariusz Jędrzejczyk <dariusz.jedrzejczyk@broadcom.com>
2025-02-17 10:54:27 +0100

    [build] Compile with recent javac but target JDK 8
    Using Gradle toolchains and mrjar plugin it is difficult to use the
    latest JDK to compile against JDK 8 but with the latest compiler. This
    change enhances the JavaConventions internal plugin to allow taking
    advantage of more recent features but compiling appropriate byte code
    levels. This also means the java21 and related version-specific tasks
    are compile with the relevant version and tests are run with the
    required version.
    
    toolchains.gradle configuration was removed as it was not used and made
    things more difficult at this point by colliding with the above
    enhancement when used.

--- buildSrc/src/main/java/io/reactor/gradle/JavaConventions.java
+++ buildSrc/src/main/java/io/reactor/gradle/JavaConventions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2024 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2020-2025 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,17 +20,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.file.FileCollection;
 import org.gradle.api.plugins.JavaPlugin;
-import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.compile.JavaCompile;
+import org.gradle.api.tasks.testing.Test;
+import org.gradle.jvm.toolchain.JavaCompiler;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
+import org.gradle.jvm.toolchain.JavaToolchainService;
 
 /**
  * @author Simon Baslé
+ * @author Dariusz Jędrzejczyk
  */
 public class JavaConventions implements Plugin<Project> {
+
+	private final JavaToolchainService javaToolchains;
+
+	@Inject
+	JavaConventions(JavaToolchainService javaToolchains) {
+		this.javaToolchains = javaToolchains;
+	}
 
 	@Override
 	public void apply(Project project) {
@@ -38,44 +52,90 @@ public class JavaConventions implements Plugin<Project> {
 	}
 
 	private void applyJavaConvention(Project project) {
-		project.getExtensions().getByType(JavaPluginExtension.class).toolchain(toolchain -> {
-			toolchain.getLanguageVersion().set(JavaLanguageVersion.of(8));
+		project.afterEvaluate(p -> {
+			p.getTasks()
+			 .withType(JavaCompile.class)
+			 .forEach(compileTask -> {
+				 compileTask.getJavaCompiler().set(javaToolchains.compilerFor(spec -> spec.getLanguageVersion().set(JavaLanguageVersion.of(21))));
+
+				 compileTask.getOptions().setEncoding("UTF-8");
+
+				 setJavaRelease(compileTask, project);
+				 List<String> compilerArgs = new ArrayList<>(compileTask.getOptions()
+				                                                        .getCompilerArgs());
+
+				 if (compileTask.getName().endsWith("TestJava")) {
+					 compilerArgs.add("-parameters");
+				 }
+				 compilerArgs.addAll(Arrays.asList(
+						 "-Xlint:-varargs",
+						 // intentionally disabled
+						 "-Xlint:cast",
+						 "-Xlint:classfile",
+						 "-Xlint:dep-ann",
+						 "-Xlint:divzero",
+						 "-Xlint:empty",
+						 "-Xlint:finally",
+						 "-Xlint:overrides",
+						 "-Xlint:path",
+						 "-Xlint:-processing",
+						 "-Xlint:static",
+						 "-Xlint:try",
+						 "-Xlint:deprecation",
+						 "-Xlint:unchecked",
+						 "-Xlint:-serial",// intentionally disabled
+						 "-Xlint:-options",// intentionally disabled
+						 "-Xlint:-fallthrough",// intentionally disabled
+						 "-Xmaxerrs", "500",
+						 "-Xmaxwarns", "1000"
+				 ));
+
+				 compileTask.getOptions().setCompilerArgs(compilerArgs);
+			 });
 		});
 
-		project.getTasks()
-			.withType(JavaCompile.class)
-			.forEach(compileTask -> {
-				compileTask.getOptions().setEncoding("UTF-8");
+		project.afterEvaluate(p -> {
+			p.getTasks()
+			 .withType(Test.class)
+			 .forEach(testTask -> {
+				 int version = testTask.getJavaLauncher().get().getMetadata().getLanguageVersion().asInt();
+				 System.out.println(testTask.getName() + ": running with Java " + version);
+			 });
+		});
+	}
 
-				List<String> compilerArgs = new ArrayList<>(
-						compileTask.getOptions().getCompilerArgs());
+	private void setJavaRelease(JavaCompile task, Project project) {
+		int defaultVersion = 8;
+		int releaseVersion = defaultVersion;
+		int compilerVersion = task.getJavaCompiler().get().getMetadata().getLanguageVersion().asInt();
 
-				if (compileTask.getName().endsWith("TestJava")) {
-					compilerArgs.add("-parameters");
-				}
-				compilerArgs.addAll(Arrays.asList(
-					"-Xlint:-varargs", // intentionally disabled
-					"-Xlint:cast",
-					"-Xlint:classfile",
-					"-Xlint:dep-ann",
-					"-Xlint:divzero",
-					"-Xlint:empty",
-					"-Xlint:finally",
-					"-Xlint:overrides",
-					"-Xlint:path",
-					"-Xlint:-processing",
-					"-Xlint:static",
-					"-Xlint:try",
-					"-Xlint:deprecation",
-					"-Xlint:unchecked",
-					"-Xlint:-serial",      // intentionally disabled
-					"-Xlint:-options",     // intentionally disabled
-					"-Xlint:-fallthrough", // intentionally disabled
-					"-Xmaxerrs", "500",
-					"-Xmaxwarns", "1000"
-				));
+		for (int version = defaultVersion ; version <= compilerVersion ; version++) {
+			if (task.getName().contains("Java" + version)) {
+				releaseVersion = version;
+				break;
+			}
+		}
+		final int finalVersion = releaseVersion;
 
-				compileTask.getOptions().setCompilerArgs(compilerArgs);
-			});
+		System.out.println(task.getName() + ": compiling with Java " + compilerVersion + ", targeting " + finalVersion);
+
+		Provider<JavaCompiler> compiler = project.getExtensions()
+		                                         .getByType(JavaToolchainService.class)
+		                                         .compilerFor(spec -> spec.getLanguageVersion().set(JavaLanguageVersion.of(finalVersion)));
+
+		// We can't use --release for Java8: https://bugs.openjdk.org/browse/JDK-8206937
+		// task.getOptions().getRelease().set(releaseVersion);
+
+		String releaseVersionString = JavaLanguageVersion.of(finalVersion).toString();
+		task.setSourceCompatibility(releaseVersionString);
+		task.setTargetCompatibility(releaseVersionString);
+		FileCollection existingBootClasspath = task.getOptions().getBootstrapClasspath();
+		FileCollection bootClasspath = project.files(compiler.map(c -> c.getMetadata().getInstallationPath().getAsFileTree()).get());
+
+		if (existingBootClasspath != null) {
+			bootClasspath = existingBootClasspath.plus(bootClasspath);
+		}
+
+		task.getOptions().setBootstrapClasspath(bootClasspath);
 	}
 }
