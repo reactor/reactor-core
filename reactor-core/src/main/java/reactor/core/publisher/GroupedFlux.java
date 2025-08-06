db24a452d0efd94f306dd5f75085ce3d1e31b21f
Dariusz Jędrzejczyk <dariusz.jedrzejczyk@broadcom.com>
2025-08-06 15:58:42 +0200

    WIP: Update copyright headers
    Signed-off-by: Dariusz Jędrzejczyk <dariusz.jedrzejczyk@broadcom.com>

--- reactor-core/src/main/java/reactor/core/publisher/GroupedFlux.java
+++ reactor-core/src/main/java/reactor/core/publisher/GroupedFlux.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2021 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2015-2025 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
